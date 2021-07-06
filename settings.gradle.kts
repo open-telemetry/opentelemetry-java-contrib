5cc74891d665d695188badeeaac3e4eaa49fafd7
Anuraag Agrawal <aanuraag@amazon.co.jp>
2021-07-06 13:34:04 +0900

    Update to Gradle 7.1 (#47)
    * Update to Gradle 7.1
    
    * Fix

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 pluginManagement {
     plugins {
         id("com.github.ben-manes.versions") version "0.39.0"
-        id("com.github.johnrengelman.shadow") version "6.0.0"
+        id("com.github.johnrengelman.shadow") version "7.0.0"
         id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
     }
 }
