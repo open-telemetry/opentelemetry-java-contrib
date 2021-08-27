e58533331fd1d28f8dd342b0c1bfb275750bdecf
Nikita Salnikov-Tarnovski <gnikem@gmail.com>
2021-08-27 15:08:47 +0300

    Better equals

--- contrib-samplers/src/main/java/io/opentelemetry/contrib/samplers/SamplingRule.java
+++ contrib-samplers/src/main/java/io/opentelemetry/contrib/samplers/SamplingRule.java
@@ -36,7 +36,7 @@ class SamplingRule {
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
+    if (!(o instanceof SamplingRule)) return false;
     SamplingRule that = (SamplingRule) o;
     return attributeKey.equals(that.attributeKey) && pattern.equals(that.pattern);
   }
