63be26a22aa9fa02ef885df476439ff722c03cb4
Ben Evans <beevans@redhat.com>
2021-09-21 16:06:05 +0200

    Initial commit

---
+++ jfr-streaming/src/main/java/org/jfr/metrics/ThreadGrouper.java
@@ -0,0 +1,17 @@
+package org.jfr.metrics;
+
+import jdk.jfr.consumer.RecordedEvent;
+import jdk.jfr.consumer.RecordedThread;
+
+import java.util.Optional;
+
+public class ThreadGrouper {
+    // FIXME doesn't actually do any grouping, but should be safe for now
+    public Optional<String> groupedName(RecordedEvent ev) {
+        Object thisField = ev.getValue("eventThread");
+        if (thisField != null && thisField instanceof RecordedThread) {
+            return Optional.of(((RecordedThread) thisField).getJavaName());
+        }
+        return Optional.empty();
+    }
+}
