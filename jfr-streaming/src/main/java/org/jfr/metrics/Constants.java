cdb565e3b8a2ee2416c77b0c371667a0ce461823
Ben Evans <ben@kathik.co.uk>
2021-10-07 17:05:59 +0200

    Debug build issue

---
+++ jfr-streaming/src/main/java/org/jfr/metrics/Constants.java
@@ -0,0 +1,9 @@
+package org.jfr.metrics;
+
+public class Constants {
+    private Constants() {}
+
+    public static final String THREAD_NAME = "thread.name";
+    public static final String KILOBYTES = "KB";
+    public static final String MILLISECONDS = "ms";
+}
