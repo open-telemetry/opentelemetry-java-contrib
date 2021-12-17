34adb69fbfe489c9c6d9e38e52ff8faf5c0720dd
Cyrille Le Clerc <cyrille.leclerc@elastic.co>
2021-12-17 14:19:28 +0100

    spotless apply

--- maven-extension/src/test/java/io/opentelemetry/maven/handler/MojoGoalExecutionHandlerConfigurationTest.java
+++ maven-extension/src/test/java/io/opentelemetry/maven/handler/MojoGoalExecutionHandlerConfigurationTest.java
@@ -9,10 +9,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import io.opentelemetry.maven.MavenGoal;
 import io.opentelemetry.maven.OtelExecutionListener;
-import io.opentelemetry.maven.handler.MojoGoalExecutionHandler;
-import java.util.Collection;
 import java.util.Map;
-import io.opentelemetry.maven.handler.MojoGoalExecutionHandlerConfiguration;
 import org.junit.jupiter.api.Test;
 
 public class MojoGoalExecutionHandlerConfigurationTest {
@@ -20,8 +17,9 @@ public class MojoGoalExecutionHandlerConfigurationTest {
   @Test
   public void mojoGoalExecutionHandlers() {
 
-    final Map<MavenGoal, MojoGoalExecutionHandler> actual = MojoGoalExecutionHandlerConfiguration.loadMojoGoalExecutionHandler(
-        OtelExecutionListener.class.getClassLoader());
+    final Map<MavenGoal, MojoGoalExecutionHandler> actual =
+        MojoGoalExecutionHandlerConfiguration.loadMojoGoalExecutionHandler(
+            OtelExecutionListener.class.getClassLoader());
     assertThat(actual.size()).isEqualTo(5);
   }
 }
