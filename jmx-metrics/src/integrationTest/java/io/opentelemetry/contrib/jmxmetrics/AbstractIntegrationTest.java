b2733faa6aad189c7930fda6cb03aa9d86110138
Mrod1598 <miguel.rodriguez@observiq.com>
2021-12-17 11:07:56 -0500

    Fix timeout

--- jmx-metrics/src/integrationTest/java/io/opentelemetry/contrib/jmxmetrics/AbstractIntegrationTest.java
+++ jmx-metrics/src/integrationTest/java/io/opentelemetry/contrib/jmxmetrics/AbstractIntegrationTest.java
@@ -110,7 +110,7 @@ public abstract class AbstractIntegrationTest {
 
   protected void waitAndAssertMetrics(Consumer<Metric>... assertions) {
     await()
-        .atMost(Duration.ofSeconds(200))
+        .atMost(Duration.ofSeconds(30))
         .untilAsserted(
             () -> {
               List<Metric> metrics =
