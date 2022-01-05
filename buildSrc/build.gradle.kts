743a0d3d0887561d8d569b56e14b7745664b9825
Anuraag Agrawal <anuraaga@gmail.com>
2022-01-05 18:30:23 +0900

    Format gradle scripts (#168)
    * Add formatting of buildSrc
    
    * Format root with conventions
    
    * Format top level
    
    * Format

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,18 +1,25 @@
 plugins {
-    `kotlin-dsl`
-    // When updating, update below in dependencies too
-    id("com.diffplug.spotless") version "6.0.5"
+  `kotlin-dsl`
+  // When updating, update below in dependencies too
+  id("com.diffplug.spotless") version "6.0.5"
 }
 
 repositories {
-    mavenCentral()
-    gradlePluginPortal()
-    mavenLocal()
+  mavenCentral()
+  gradlePluginPortal()
+  mavenLocal()
 }
 
 dependencies {
-    // When updating, update above in plugins too
-    implementation("com.diffplug.spotless:spotless-plugin-gradle:6.0.5")
-    implementation("net.ltgt.gradle:gradle-errorprone-plugin:2.0.2")
-    implementation("net.ltgt.gradle:gradle-nullaway-plugin:1.2.0")
+  // When updating, update above in plugins too
+  implementation("com.diffplug.spotless:spotless-plugin-gradle:6.0.5")
+  implementation("net.ltgt.gradle:gradle-errorprone-plugin:2.0.2")
+  implementation("net.ltgt.gradle:gradle-nullaway-plugin:1.2.0")
+}
+
+spotless {
+  kotlinGradle {
+    ktlint().userData(mapOf("indent_size" to "2", "continuation_indent_size" to "2", "disabled_rules" to "no-wildcard-imports"))
+    target("**/*.gradle.kts")
+  }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,12 @@
 pluginManagement {
-    plugins {
-        id("com.github.ben-manes.versions") version "0.39.0"
-        id("com.github.johnrengelman.shadow") version "7.1.1"
-        id("com.gradle.enterprise") version "3.8"
-        id("org.unbroken-dome.test-sets") version "4.0.0"
-        id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
-        id("nebula.release") version "16.0.0"
-    }
+  plugins {
+    id("com.github.ben-manes.versions") version "0.39.0"
+    id("com.github.johnrengelman.shadow") version "7.1.1"
+    id("com.gradle.enterprise") version "3.8"
+    id("org.unbroken-dome.test-sets") version "4.0.0"
+    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
+    id("nebula.release") version "16.0.0"
+  }
 }
 
 plugins {
@@ -14,24 +14,24 @@ plugins {
 }
 
 dependencyResolutionManagement {
-    repositories {
-        mavenCentral()
-        mavenLocal()
-    }
+  repositories {
+    mavenCentral()
+    mavenLocal()
+  }
 }
 
 val isCI = System.getenv("CI") != null
 val skipBuildscan = System.getenv("SKIP_BUILDSCAN").toBoolean()
 gradleEnterprise {
-    buildScan {
-        termsOfServiceUrl = "https://gradle.com/terms-of-service"
-        termsOfServiceAgree = "yes"
+  buildScan {
+    termsOfServiceUrl = "https://gradle.com/terms-of-service"
+    termsOfServiceAgree = "yes"
 
-        if (isCI && !skipBuildscan) {
-            publishAlways()
-            tag("CI")
-        }
+    if (isCI && !skipBuildscan) {
+      publishAlways()
+      tag("CI")
     }
+  }
 }
 
 rootProject.name = "opentelemetry-java-contrib"
