c6fda02af24ca1b70742a7e9d71ac61e9e49e30b
Anuraag Agrawal <anuraaga@gmail.com>
2022-01-05 08:35:37 +0000

    Format top level

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,53 +2,42 @@ import nebula.plugin.release.git.opinion.Strategies
 import java.time.Duration
 
 plugins {
-    id("otel.spotless-conventions")
+  id("otel.spotless-conventions")
 
-    id("idea")
+  id("idea")
 
-    id("com.github.ben-manes.versions")
-    id("io.github.gradle-nexus.publish-plugin")
-    id("nebula.release")
+  id("com.github.ben-manes.versions")
+  id("io.github.gradle-nexus.publish-plugin")
+  id("nebula.release")
 }
 
 release {
-    defaultVersionStrategy = Strategies.getSNAPSHOT()
+  defaultVersionStrategy = Strategies.getSNAPSHOT()
 }
 
 nebulaRelease {
-    addReleaseBranchPattern("""v\d+\.\d+\.x""")
+  addReleaseBranchPattern("""v\d+\.\d+\.x""")
 }
 
 nexusPublishing {
-    packageGroup.set("io.opentelemetry")
+  packageGroup.set("io.opentelemetry")
 
-    repositories {
-        sonatype {
-            username.set(System.getenv("SONATYPE_USER"))
-            password.set(System.getenv("SONATYPE_KEY"))
-        }
+  repositories {
+    sonatype {
+      username.set(System.getenv("SONATYPE_USER"))
+      password.set(System.getenv("SONATYPE_KEY"))
     }
+  }
 
-    connectTimeout.set(Duration.ofMinutes(5))
-    clientTimeout.set(Duration.ofMinutes(5))
+  connectTimeout.set(Duration.ofMinutes(5))
+  clientTimeout.set(Duration.ofMinutes(5))
 
-    transitionCheckOptions {
-        // We have many artifacts so Maven Central takes a long time on its compliance checks. This sets
-        // the timeout for waiting for the repository to close to a comfortable 50 minutes.
-        maxRetries.set(300)
-        delayBetween.set(Duration.ofSeconds(10))
-    }
+  transitionCheckOptions {
+    // We have many artifacts so Maven Central takes a long time on its compliance checks. This sets
+    // the timeout for waiting for the repository to close to a comfortable 50 minutes.
+    maxRetries.set(300)
+    delayBetween.set(Duration.ofSeconds(10))
+  }
 }
 
 description = "OpenTelemetry Contrib libraries and utilities for the JVM"
-
-spotless {
-    // this formatting is applied at the root level, as some of these files are not in a submodules
-    // and would be missed otherwise
-    format("misc") {
-        target(".gitignore", "*.md", "docs/**/*.md")
-        indentWithSpaces()
-        trimTrailingWhitespace()
-        endWithNewline()
-    }
-}
