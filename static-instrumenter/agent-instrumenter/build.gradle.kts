47ba0892dda1068e0d8924fe6e73be9449694a55
Jakub Wach <jwach@splunk.com>
2022-03-08 14:06:02 +0100

    Apply suggestions from code review
    Co-authored-by: Anuraag Agrawal <anuraaga@gmail.com>

--- static-instrumenter/agent-instrumenter/build.gradle.kts
+++ static-instrumenter/agent-instrumenter/build.gradle.kts
@@ -4,13 +4,7 @@ plugins {
 
 description = "OpenTelemetry Java Static Instrumentation Agent"
 
-repositories {
-  mavenLocal()
-  mavenCentral()
-}
-
 dependencies {
   implementation("org.slf4j:slf4j-api")
-  implementation("org.slf4j:slf4j-simple")
-  testImplementation("org.mockito:mockito-core:3.+")
+  runtimeOnly("org.slf4j:slf4j-simple")
 }
