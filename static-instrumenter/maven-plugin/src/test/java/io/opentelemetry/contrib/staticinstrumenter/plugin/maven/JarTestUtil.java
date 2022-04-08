c3c352cd3087668d55582676cc5dd79fc6556a86
Jakub Wach <jwach@splunk.com>
2022-04-08 12:42:49 +0200

    tests added

---
+++ static-instrumenter/maven-plugin/src/test/java/io/opentelemetry/contrib/staticinstrumenter/plugin/maven/JarTestUtil.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.contrib.staticinstrumenter.plugin.maven;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.jar.JarFile;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+final class JarTestUtil {
+
+  private JarTestUtil() {}
+
+  static void assertJar(Path jarFile, String... files) throws IOException {
+    assertThat(Files.exists(jarFile)).isTrue();
+    try (JarFile jar = new JarFile(jarFile.toFile())) {
+      for (int i = 0; i < files.length; i++) {
+        String file = files[i];
+        ZipEntry entry = jar.getEntry(file);
+        assertThat(entry).isNotNull();
+      }
+    }
+  }
+
+  static String getResourcePath(String fileName) {
+    return JarTestUtil.class.getClassLoader().getResource(fileName).getFile();
+  }
+
+  @FunctionalInterface
+  interface ThrowingConsumer<T, E extends Exception> {
+    void accept(T t) throws E;
+  }
+
+  static Path createJar(String prefix, ThrowingConsumer<ZipOutputStream, IOException> creator)
+      throws IOException {
+    Path path = Files.createTempFile(prefix, "jar");
+    try (ZipOutputStream out = new ZipOutputStream(Files.newOutputStream(path))) {
+      creator.accept(out);
+    }
+    return path;
+  }
+}
---
+++ static-instrumenter/maven-plugin/src/test/java/io/opentelemetry/contrib/staticinstrumenter/plugin/maven/ZipEntryCreatorTest.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.contrib.staticinstrumenter.plugin.maven;
+
+import static io.opentelemetry.contrib.staticinstrumenter.plugin.maven.JarTestUtil.assertJar;
+import static io.opentelemetry.contrib.staticinstrumenter.plugin.maven.JarTestUtil.createJar;
+import static io.opentelemetry.contrib.staticinstrumenter.plugin.maven.JarTestUtil.getResourcePath;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.jar.JarFile;
+import org.junit.jupiter.api.Test;
+
+class ZipEntryCreatorTest {
+
+  @Test
+  void shouldMoveEntryWithoutRenaming() throws Exception {
+    // given
+    JarFile source = new JarFile(getResourcePath("test.jar"));
+    // when
+    Path targetFile =
+        createJar(
+            "test-copy",
+            (target) ->
+                ZipEntryCreator.moveEntry(
+                    target,
+                    "test/TestClass.class",
+                    source.getJarEntry("test/TestClass.class"),
+                    source));
+    // then
+    assertJar(targetFile, "test/TestClass.class");
+  }
+
+  @Test
+  void shouldMoveEntryRenaming() throws Exception {
+    // given
+    JarFile source = new JarFile(getResourcePath("test.jar"));
+    // when
+    Path targetFile =
+        createJar(
+            "test-copy",
+            (target) -> {
+              ZipEntryCreator.moveEntry(
+                  target,
+                  "newpath/TestClassRenamed.classdata",
+                  source.getJarEntry("test/TestClass.class"),
+                  source);
+              ZipEntryCreator.moveEntry(
+                  target,
+                  "test/NotInstrumented.class",
+                  source.getJarEntry("test/NotInstrumented.class"),
+                  source);
+            });
+    // then
+    assertJar(targetFile, "newpath/TestClassRenamed.classdata", "test/NotInstrumented.class");
+  }
+
+  @Test
+  void shouldAddFileToJar() throws Exception {
+    // given
+    Path file = Paths.get(getResourcePath("testing.file"));
+    // when
+    Path targetFile =
+        createJar(
+            "new-jar",
+            (target) -> ZipEntryCreator.createZipEntryFromFile(target, file, "stored/entry.file"));
+    // then
+    assertJar(targetFile, "stored/entry.file");
+  }
+}
