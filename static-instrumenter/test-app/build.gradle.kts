1474dff9d906328169f40b428d1816e7f9c57985
Jakub Wach <jwach@splunk.com>
2022-04-27 01:13:02 +0200

    small test app for verifying instrumentation (#313)
    * small test app for verifying instrumentation
    
    * Update static-instrumenter/test-app/build.gradle.kts
    
    Co-authored-by: Trask Stalnaker <trask.stalnaker@gmail.com>
    
    * code review
    
    Co-authored-by: Trask Stalnaker <trask.stalnaker@gmail.com>

---
+++ static-instrumenter/test-app/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+  id("otel.java-conventions")
+  id("com.github.johnrengelman.shadow")
+}
+
+description = "OpenTelemetry Java Static Instrumentation Test Application"
+
+dependencies {
+  implementation("org.apache.httpcomponents:httpclient:4.5.13")
+  implementation("org.slf4j:slf4j-api")
+  runtimeOnly("org.slf4j:slf4j-simple")
+}
+
+tasks {
+  withType<JavaCompile>().configureEach {
+    with(options) {
+      release.set(11)
+    }
+  }
+
+  jar {
+    manifest {
+      attributes("Main-Class" to "io.opentelemetry.contrib.staticinstrumenter.test.HttpClientTest")
+    }
+  }
+
+  assemble {
+    dependsOn(shadowJar)
+  }
+}
