5e071622f9a8add470169f150dc14f85a901dfe3
Jean Bisutti <jean.bisutti@gmail.com>
2022-05-03 14:31:20 +0200

    Add a test on runtime attachment

--- runtime-attach/build.gradle.kts
+++ runtime-attach/build.gradle.kts
@@ -11,4 +11,10 @@ dependencies {
 
   // Used by byte-buddy but not brought in as a transitive dependency.
   compileOnly("com.google.code.findbugs:annotations")
+
+  testImplementation("io.opentelemetry.javaagent:opentelemetry-javaagent:1.6.0")
+  testImplementation("org.junit.jupiter:junit-jupiter-api")
+  testImplementation("org.junit-pioneer:junit-pioneer")
+  testImplementation("org.assertj:assertj-core")
+  testImplementation("org.apache.httpcomponents:httpclient:4.5.13")
 }
---
+++ runtime-attach/src/test/java/AgentAttachmentTest.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.opentelemetry.contrib.attach.RuntimeAttach;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.apache.http.Header;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.junit.jupiter.api.Test;
+import org.junitpioneer.jupiter.SetEnvironmentVariable;
+
+class AgentAttachmentTest {
+
+  @Test
+  @SetEnvironmentVariable(key = "OTEL_TRACES_EXPORTER", value = "logging")
+  void attachOtelAgent() throws IOException {
+
+    RuntimeAttach.attachJavaagentToCurrentJVM();
+
+    List<String> httpHeaders = makeHttpCallAndReturnHeaders();
+
+    String w3cTraceParentHeader = "traceparent";
+
+    assertThat(httpHeaders).contains(w3cTraceParentHeader);
+  }
+
+  private static List<String> makeHttpCallAndReturnHeaders() throws IOException {
+    try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+      HttpGet request = new HttpGet("https://httpbin.org/get");
+      try (CloseableHttpResponse ignored = httpClient.execute(request)) {
+        return Arrays.stream(request.getAllHeaders())
+            .map(Header::getName)
+            .collect(Collectors.toList());
+      }
+    }
+  }
+}
