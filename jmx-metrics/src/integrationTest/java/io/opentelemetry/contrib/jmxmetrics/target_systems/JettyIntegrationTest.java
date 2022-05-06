69995bea676c174c356250b1d23aa89afc3c8907
Mrod1598 <miguel.rodriguez@observiq.com>
2022-05-06 08:44:24 -0400

    Spotless

--- jmx-metrics/src/integrationTest/java/io/opentelemetry/contrib/jmxmetrics/target_systems/JettyIntegrationTest.java
+++ jmx-metrics/src/integrationTest/java/io/opentelemetry/contrib/jmxmetrics/target_systems/JettyIntegrationTest.java
@@ -37,7 +37,8 @@ class JettyIntegrationTest extends AbstractIntegrationTest {
                                   "--add-to-startd=jmx,stats,http")
                               .run("mkdir -p /var/lib/jetty/webapps/ROOT/")
                               .run("touch /var/lib/jetty/webapps/ROOT/index.html")
-                              .env("JAVA_OPTIONS",
+                              .env(
+                                  "JAVA_OPTIONS",
                                   "-Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.port=1099 -Dcom.sun.management.jmxremote.rmi.port=1099")
                               .build()))
           .withNetwork(Network.SHARED)
