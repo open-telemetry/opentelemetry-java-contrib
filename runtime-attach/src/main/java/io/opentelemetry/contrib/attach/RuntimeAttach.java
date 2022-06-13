37cf1e5b6ba4eac56659834fdc435911e3231e25
jeanbisutti <jean.bisutti@gmail.com>
2022-06-13 15:32:11 +0200

    Add log when agent is disabled with otel.javaagent.enabled property or OTEL_JAVAAGENT_ENABLED environment variable

--- runtime-attach/src/main/java/io/opentelemetry/contrib/attach/RuntimeAttach.java
+++ runtime-attach/src/main/java/io/opentelemetry/contrib/attach/RuntimeAttach.java
@@ -7,22 +7,43 @@ package io.opentelemetry.contrib.attach;
 
 import java.io.File;
 import java.lang.management.ManagementFactory;
+import java.util.logging.Logger;
 import net.bytebuddy.agent.ByteBuddyAgent;
 
 public final class RuntimeAttach {
 
+  private static final Logger LOGGER = Logger.getLogger(RuntimeAttach.class.getName());
+  private static final String AGENT_ENABLED_PROPERTY = "otel.javaagent.enabled";
+  private static final String AGENT_ENABLED_ENV_VAR = "OTEL_JAVAAGENT_ENABLED";
+
   public static void attachJavaagentToCurrentJVM() {
-    if (agentIsDisabled()) {
+    if (!shouldAttach()) {
       return;
     }
     File javaagentFile = AgentFileLocator.locateAgentFile();
     ByteBuddyAgent.attach(javaagentFile, getPid());
   }
 
-  private static boolean agentIsDisabled() {
-    String enabledProperty =
-        System.getProperty("otel.javaagent.enabled", System.getenv("OTEL_JAVAAGENT_ENABLED"));
-    return "false".equals(enabledProperty);
+  private static boolean shouldAttach() {
+    if (agentIsDisabledWithProp()) {
+      LOGGER.warning("Agent was disabled with " + AGENT_ENABLED_PROPERTY + " property.");
+      return false;
+    }
+    if (agentIsDisabledWithEnvVar()) {
+      LOGGER.warning("Agent was disabled with " + AGENT_ENABLED_ENV_VAR + " environment variable.");
+      return false;
+    }
+    return true;
+  }
+
+  private static boolean agentIsDisabledWithProp() {
+    String agentEnabledPropValue = System.getProperty(AGENT_ENABLED_PROPERTY);
+    return "false".equals(agentEnabledPropValue);
+  }
+
+  private static boolean agentIsDisabledWithEnvVar() {
+    String agentEnabledEnvVarValue = System.getenv(AGENT_ENABLED_ENV_VAR);
+    return "false".equals(agentEnabledEnvVarValue);
   }
 
   private static String getPid() {
