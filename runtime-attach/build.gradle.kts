658daade557fa828518be665512850f89d193033
Jean Bisutti <jean.bisutti@gmail.com>
2022-06-17 22:13:17 +0200

    Runtime attachment additions (#354)
    * Add tests on runtime attachment
    
    * Add log when agent is disabled with otel.javaagent.enabled property or OTEL_JAVAAGENT_ENABLED environment variable
    
    * Do not try to attach if agent is already attached
    
    * Do not try to attach if attachment is not asked for the main thread
    
    * Add log if an unexpected issue has happened during attachment
    
    * Add javadoc
    
    * Add information to the Readme
    
    * Format javadoc
    
    * Fix mistake in Readme
    
    * Update runtime-attach/build.gradle.kts
    
    Co-authored-by: Trask Stalnaker <trask.stalnaker@gmail.com>
    
    * Update runtime-attach/README.md
    
    Co-authored-by: Trask Stalnaker <trask.stalnaker@gmail.com>
    
    * Update runtime-attach/src/main/java/io/opentelemetry/contrib/attach/RuntimeAttach.java
    
    Co-authored-by: Trask Stalnaker <trask.stalnaker@gmail.com>
    
    * Update runtime-attach/src/main/java/io/opentelemetry/contrib/attach/RuntimeAttach.java
    
    Co-authored-by: Trask Stalnaker <trask.stalnaker@gmail.com>
    
    * Update runtime-attach/src/main/java/io/opentelemetry/contrib/attach/RuntimeAttach.java
    
    Co-authored-by: Trask Stalnaker <trask.stalnaker@gmail.com>
    
    * Update runtime-attach/build.gradle.kts
    
    Co-authored-by: Trask Stalnaker <trask.stalnaker@gmail.com>
    
    * Replace triple ``` by single `
    
    * Log at debug level in shouldAttach() method
    
    * Log at warn level if the runtime attachment is not requested from main thread
    
    * Logger name in lower case as in OTel agent
    
    * Add space in class Javadoc
    
    * Add space in Readme
    
    * Update Javadoc of method
    
    * Update runtime-attach/src/main/java/io/opentelemetry/contrib/attach/RuntimeAttach.java
    
    Co-authored-by: Trask Stalnaker <trask.stalnaker@gmail.com>
    
    * Don't attach if attachment is not requested from the main method
    
    * Inline local variable
    
    * Move opentelemetry-javaagent to dependency management
    
    Co-authored-by: Trask Stalnaker <trask.stalnaker@gmail.com>

--- runtime-attach/build.gradle.kts
+++ runtime-attach/build.gradle.kts
@@ -11,4 +11,15 @@ dependencies {
 
   // Used by byte-buddy but not brought in as a transitive dependency.
   compileOnly("com.google.code.findbugs:annotations")
+
+  testImplementation("io.opentelemetry.javaagent:opentelemetry-javaagent")
+  testImplementation("io.opentelemetry:opentelemetry-extension-annotations")
+  testImplementation("org.junit.jupiter:junit-jupiter-api")
+  testImplementation("org.junit-pioneer:junit-pioneer")
+  testImplementation("org.assertj:assertj-core")
+}
+
+tasks.test {
+  useJUnitPlatform()
+  setForkEvery(1) // One JVM by test class to avoid a test class launching a runtime attachment influences the behavior of another test class
 }
---
+++ runtime-attach/src/test/java/io/opentelemetry/contrib/attach/AbstractAttachmentTest.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.contrib.attach;
+
+import io.opentelemetry.javaagent.shaded.io.opentelemetry.api.trace.Span;
+import org.junit.jupiter.api.BeforeAll;
+
+public class AbstractAttachmentTest {
+
+  @BeforeAll
+  static void disableMainThreadCheck() {
+    System.setProperty(RuntimeAttach.MAIN_METHOD_CHECK_PROP, "false");
+  }
+
+  boolean isAttached() {
+    return Span.current().getSpanContext().isValid();
+  }
+}
