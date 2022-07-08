c807a9defe4a98fac3674474332fae72df4e34b7
Trask Stalnaker <trask.stalnaker@gmail.com>
2022-07-08 12:40:36 -0700

    Move runtime attach modules under single top-level directory

--- runtime-attach/runtime-attach/build.gradle.kts
+++ runtime-attach/runtime-attach/build.gradle.kts
@@ -8,7 +8,7 @@ description = "To runtime attach the OpenTelemetry Java Instrumentation agent"
 val agent: Configuration by configurations.creating
 
 dependencies {
-  implementation(project(":runtime-attach-core"))
+  implementation(project(":runtime-attach:runtime-attach-core"))
   agent("io.opentelemetry.javaagent:opentelemetry-javaagent:1.15.0")
 
   // Used by byte-buddy but not brought in as a transitive dependency.
