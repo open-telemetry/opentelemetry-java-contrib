3f02eedf7f37061b7aab9040ec3a41285ab358ba
Trask Stalnaker <trask.stalnaker@gmail.com>
2022-10-03 13:07:52 -0700

    Target maven 3.5.0 (#472)
    Resolves #467

--- maven-extension/build.gradle.kts
+++ maven-extension/build.gradle.kts
@@ -27,7 +27,7 @@ dependencies {
   annotationProcessor("com.google.auto.value:auto-value")
   compileOnly("com.google.auto.value:auto-value-annotations")
 
-  compileOnly("org.apache.maven:maven-core:3.5.0")
+  compileOnly("org.apache.maven:maven-core:3.5.0") // do not auto-update this version
   compileOnly("org.slf4j:slf4j-api")
   compileOnly("org.sonatype.aether:aether-api:1.13.1")
 
--- static-instrumenter/maven-plugin/build.gradle.kts
+++ static-instrumenter/maven-plugin/build.gradle.kts
@@ -11,13 +11,13 @@ description = "Maven3 plugin for static instrumentation of projects code and dep
 base.archivesName.set("static-instrumentation-maven-plugin")
 
 dependencies {
-  implementation("org.apache.maven:maven-plugin-api:3.8.6")
+  implementation("org.apache.maven:maven-plugin-api:3.5.0") // do not auto-update this version
   implementation("org.apache.maven:maven-project:2.2.1")
   compileOnly("org.apache.maven.plugin-tools:maven-plugin-annotations:3.6.4")
-  compileOnly("org.apache.maven:maven-core:3.5.0")
+  compileOnly("org.apache.maven:maven-core:3.5.0") // do not auto-update this version
   compileOnly("org.slf4j:slf4j-api")
 
-  testImplementation("org.apache.maven.plugin-tools:maven-plugin-annotations:3.6.0")
+  testImplementation("org.apache.maven.plugin-tools:maven-plugin-annotations:3.6.4")
   testImplementation("org.apache.maven:maven-core:3.5.0")
   testImplementation("org.slf4j:slf4j-simple")
 }
