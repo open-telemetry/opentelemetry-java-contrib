d277cfc10c0e7b77fcccb1d934399f5ee558aac8
jack-berg <34418638+jack-berg@users.noreply.github.com>
2022-10-19 10:50:34 -0500

    Move io.opentelemetry:opentelemetry-extension-aws to contrib (#547)
    Related to
    [open-telemetry/opentelemetry-java#4831](https://github.com/open-telemetry/opentelemetry-java/issues/4831).

---
+++ aws-xray-propagator/src/main/java/io/opentelemetry/contrib/awsxray/propagator/package-info.java
@@ -0,0 +1,5 @@
+/** OpenTelemetry AWS X-Ray propagator extension. */
+@ParametersAreNonnullByDefault
+package io.opentelemetry.contrib.awsxray.propagator;
+
+import javax.annotation.ParametersAreNonnullByDefault;
