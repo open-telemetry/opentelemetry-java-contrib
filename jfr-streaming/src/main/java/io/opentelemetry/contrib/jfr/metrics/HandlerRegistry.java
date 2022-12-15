456e7b6168a674795d68c9dc12ea17b57c5bcbf3
Robert Toyonaga <rtoyonag@redhat.com>
2022-12-15 14:41:19 -0500

    buffer handlers

--- jfr-streaming/src/main/java/io/opentelemetry/contrib/jfr/metrics/HandlerRegistry.java
+++ jfr-streaming/src/main/java/io/opentelemetry/contrib/jfr/metrics/HandlerRegistry.java
@@ -6,15 +6,22 @@
 package io.opentelemetry.contrib.jfr.metrics;
 
 import io.opentelemetry.api.metrics.MeterProvider;
+import io.opentelemetry.contrib.jfr.metrics.internal.GarbageCollection.G1GarbageCollectionHandler;
+import io.opentelemetry.contrib.jfr.metrics.internal.GarbageCollection.OldGarbageCollectionHandler;
+import io.opentelemetry.contrib.jfr.metrics.internal.GarbageCollection.YoungGarbageCollectionHandler;
 import io.opentelemetry.contrib.jfr.metrics.internal.RecordedEventHandler;
 import io.opentelemetry.contrib.jfr.metrics.internal.ThreadGrouper;
+import io.opentelemetry.contrib.jfr.metrics.internal.buffer.DirectBufferStatisticsHandler;
 import io.opentelemetry.contrib.jfr.metrics.internal.classes.ClassesLoadedHandler;
 import io.opentelemetry.contrib.jfr.metrics.internal.container.ContainerConfigurationHandler;
 import io.opentelemetry.contrib.jfr.metrics.internal.cpu.ContextSwitchRateHandler;
 import io.opentelemetry.contrib.jfr.metrics.internal.cpu.LongLockHandler;
 import io.opentelemetry.contrib.jfr.metrics.internal.cpu.OverallCPULoadHandler;
+import io.opentelemetry.contrib.jfr.metrics.internal.memory.CodeCacheConfigurationHandler;
 import io.opentelemetry.contrib.jfr.metrics.internal.memory.G1HeapSummaryHandler;
+import io.opentelemetry.contrib.jfr.metrics.internal.memory.GCHeapConfigurationHandler;
 import io.opentelemetry.contrib.jfr.metrics.internal.memory.GCHeapSummaryHandler;
+import io.opentelemetry.contrib.jfr.metrics.internal.memory.MetaspaceSummaryHandler;
 import io.opentelemetry.contrib.jfr.metrics.internal.memory.ObjectAllocationInNewTLABHandler;
 import io.opentelemetry.contrib.jfr.metrics.internal.memory.ObjectAllocationOutsideTLABHandler;
 import io.opentelemetry.contrib.jfr.metrics.internal.memory.ParallelHeapSummaryHandler;
@@ -28,16 +35,18 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.Supplier;
 
-final class HandlerRegistry {
+public final class HandlerRegistry {
   private static final String INSTRUMENTATION_NAME = "io.opentelemetry.contrib.jfr";
   private static final String INSTRUMENTATION_VERSION = "1.7.0-SNAPSHOT";
 
   private final List<RecordedEventHandler> mappers;
 
+  public static HashSet<String> garbageCollectors = new HashSet<String>();
+
   private static final Map<String, List<Supplier<RecordedEventHandler>>> HANDLERS_PER_GC =
       Map.of(
           "G1",
-          List.of(G1HeapSummaryHandler::new),
+          List.of(G1HeapSummaryHandler::new, G1GarbageCollectionHandler::new),
           "Parallel",
           List.of(ParallelHeapSummaryHandler::new));
 
@@ -50,6 +59,7 @@ final class HandlerRegistry {
     var seen = new HashSet<String>();
     for (var bean : ManagementFactory.getGarbageCollectorMXBeans()) {
       var name = bean.getName();
+      garbageCollectors.add(name);
       for (var gcType : HANDLERS_PER_GC.keySet()) {
         if (name.contains(gcType)
             && !seen.contains(gcType)
@@ -72,8 +82,11 @@ final class HandlerRegistry {
             new ContainerConfigurationHandler(),
             new LongLockHandler(grouper),
             new ThreadCountHandler(),
-            new ClassesLoadedHandler());
+            new ClassesLoadedHandler(),
+            new DirectBufferStatisticsHandler());
     handlers.addAll(basicHandlers);
+
+
 
     var meter =
         meterProvider
---
+++ jfr-streaming/src/test/java/io/opentelemetry/contrib/jfr/metrics/BufferMetricTest.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.contrib.jfr.metrics;
+
+import static io.opentelemetry.contrib.jfr.metrics.internal.Constants.ATTR_POOL;
+import static io.opentelemetry.contrib.jfr.metrics.internal.Constants.BYTES;
+import static io.opentelemetry.contrib.jfr.metrics.internal.Constants.UNIT_BUFFERS;
+
+import io.opentelemetry.api.common.Attributes;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class BufferMetricTest extends AbstractMetricsTest {
+
+  /**
+   * This is a basic test that allocates some buffers and tests to make sure the resulting JFR event
+   * was handled and turned into the expected metrics.
+   *
+   * <p>This test handles all 3 buffer related metrics defined in the OpenTelemetry Java runtime
+   * Semantic Conventions.
+   *
+   * <p>Currently JFR only has support for the "direct" buffer pool. The "mapped" and "mapped -
+   * 'non-volatile memory'" pools do not have corresponding JFR events. TODO: In the future, events
+   * should be added for these, if there is a need to capture their data in the
+   * process.runtime.jvm.buffer.count metric.
+   */
+  @Test
+  void shouldHaveJfrLoadedClassesCountEvents() throws Exception {
+    ByteBuffer buffer = ByteBuffer.allocateDirect(10000);
+    buffer.put("test".getBytes(StandardCharsets.UTF_8));
+
+    waitAndAssertMetrics(
+        metric ->
+            metric
+                .hasName("process.runtime.jvm.buffer.count")
+                .hasDescription("Number of buffers in the pool")
+                .hasUnit(UNIT_BUFFERS)
+                .hasLongSumSatisfying(
+                    sum ->
+                        sum.hasPointsSatisfying(
+                            point ->
+                                point.satisfies(
+                                    pointData -> {
+                                      Assertions.assertTrue(pointData.getValue() > 0);
+                                      Assertions.assertTrue(
+                                          pointData
+                                              .getAttributes()
+                                              .equals(Attributes.of(ATTR_POOL, "direct")));
+                                    }))),
+        metric ->
+            metric
+                .hasName("process.runtime.jvm.buffer.limit")
+                .hasDescription("Measure of total memory capacity of buffers")
+                .hasUnit(BYTES)
+                .hasLongSumSatisfying(
+                    sum ->
+                        sum.hasPointsSatisfying(
+                            point ->
+                                point.satisfies(
+                                    pointData -> {
+                                      Assertions.assertTrue(pointData.getValue() > 0);
+                                      Assertions.assertTrue(
+                                          pointData
+                                              .getAttributes()
+                                              .equals(Attributes.of(ATTR_POOL, "direct")));
+                                    }))),
+        metric ->
+            metric
+                .hasName("process.runtime.jvm.buffer.usage")
+                .hasDescription("Measure of memory used by buffers")
+                .hasUnit(BYTES)
+                .hasLongSumSatisfying(
+                    sum ->
+                        sum.hasPointsSatisfying(
+                            point ->
+                                point.satisfies(
+                                    pointData -> {
+                                      Assertions.assertTrue(pointData.getValue() > 0);
+                                      Assertions.assertTrue(
+                                          pointData
+                                              .getAttributes()
+                                              .equals(Attributes.of(ATTR_POOL, "direct")));
+                                    }))));
+  }
+}
