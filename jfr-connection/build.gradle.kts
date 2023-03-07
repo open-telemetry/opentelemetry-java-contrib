dac63afb2d3b6684cd21ad64916e4f6ff864353e
David Grieve <dagrieve@microsoft.com>
2023-03-07 11:10:27 -0500

    Address comments on build.gradle.kts

--- jfr-connection/build.gradle.kts
+++ jfr-connection/build.gradle.kts
@@ -7,14 +7,6 @@ description = "OpenTelemetry JFR Connection"
 otelJava.moduleName.set("io.opentelemetry.contrib.jfr.connection")
 
 dependencies {
-  testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
-  testImplementation("org.junit.platform:junit-platform-commons:1.9.2")
   testImplementation("org.openjdk.jmc:common:8.3.0")
   testImplementation("org.openjdk.jmc:flightrecorder:8.3.0")
-}
-
-tasks {
-  test {
-    useJUnitPlatform()
-  }
 }
