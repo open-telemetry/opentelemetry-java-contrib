d549f58380b007876cb4312852002888ef850801
Thomas Pierce <thp@amazon.com>
2023-04-27 13:37:58 -0700

    Update AttributePropagatingSpanProcessorBuilder
    In this commit, we are updating AttributePropagatingSpanProcessorBuilder to be somewhat more flexible in use. We still default to the behaviour originally implemented, but we expose the ability to specify span attributes that are used for propagation. This will make the component more reusable, while still achieving the goals required by AWS.
    Also, we are renaming Application terminology to Service terminology.

--- aws-xray/src/test/java/io/opentelemetry/contrib/awsxray/AttributePropagatingSpanProcessorTest.java
+++ aws-xray/src/test/java/io/opentelemetry/contrib/awsxray/AttributePropagatingSpanProcessorTest.java
@@ -7,6 +7,7 @@ package io.opentelemetry.contrib.awsxray;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import io.opentelemetry.api.common.AttributeKey;
 import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.SpanContext;
 import io.opentelemetry.api.trace.SpanKind;
@@ -16,68 +17,75 @@ import io.opentelemetry.api.trace.Tracer;
 import io.opentelemetry.context.Context;
 import io.opentelemetry.sdk.trace.ReadableSpan;
 import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import java.util.Arrays;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class AwsAttributePropagatingSpanProcessorTest {
+class AttributePropagatingSpanProcessorTest {
+
   private Tracer tracer;
+
+  AttributeKey<String> spanNameKey = AttributeKey.stringKey("spanName");
+  AttributeKey<String> testKey1 = AttributeKey.stringKey("key1");
+  AttributeKey<String> testKey2 = AttributeKey.stringKey("key2");
 
   @BeforeEach
   public void setup() {
     tracer =
         SdkTracerProvider.builder()
-            .addSpanProcessor(AwsAttributePropagatingSpanProcessor.create())
+            .addSpanProcessor(
+                AttributePropagatingSpanProcessor.create(
+                    spanNameKey, Arrays.asList(testKey1, testKey2)))
             .build()
             .get("awsxray");
   }
 
   @Test
-  public void testRemoteAttributesInheritance() {
+  public void testAttributesPropagation() {
     Span spanWithAppOnly = tracer.spanBuilder("parent").startSpan();
-    spanWithAppOnly.setAttribute(AwsAttributeKeys.AWS_REMOTE_APPLICATION, "testApplication");
-    validateSpanAttributesInheritance(spanWithAppOnly, null, "testApplication", null);
+    spanWithAppOnly.setAttribute(testKey1, "testValue1");
+    validateSpanAttributesInheritance(spanWithAppOnly, null, "testValue1", null);
 
     Span spanWithOpOnly = tracer.spanBuilder("parent").startSpan();
-    spanWithOpOnly.setAttribute(AwsAttributeKeys.AWS_REMOTE_OPERATION, "testOperation");
-    validateSpanAttributesInheritance(spanWithOpOnly, null, null, "testOperation");
+    spanWithOpOnly.setAttribute(testKey2, "testValue2");
+    validateSpanAttributesInheritance(spanWithOpOnly, null, null, "testValue2");
 
     Span spanWithAppAndOp = tracer.spanBuilder("parent").startSpan();
-    spanWithAppAndOp.setAttribute(AwsAttributeKeys.AWS_REMOTE_APPLICATION, "testApplication");
-    spanWithAppAndOp.setAttribute(AwsAttributeKeys.AWS_REMOTE_OPERATION, "testOperation");
-    validateSpanAttributesInheritance(spanWithAppAndOp, null, "testApplication", "testOperation");
+    spanWithAppAndOp.setAttribute(testKey1, "testValue1");
+    spanWithAppAndOp.setAttribute(testKey2, "testValue2");
+    validateSpanAttributesInheritance(spanWithAppAndOp, null, "testValue1", "testValue2");
   }
 
   @Test
-  public void testOverrideRemoteAttributes() {
+  public void testOverrideAttributes() {
     Span parentSpan = tracer.spanBuilder("parent").startSpan();
-    parentSpan.setAttribute(AwsAttributeKeys.AWS_REMOTE_APPLICATION, "testApplication");
-    parentSpan.setAttribute(AwsAttributeKeys.AWS_REMOTE_OPERATION, "testOperation");
+    parentSpan.setAttribute(testKey1, "testValue1");
+    parentSpan.setAttribute(testKey2, "testValue2");
 
     Span transmitSpans1 = createNestedSpan(parentSpan, 2);
 
     Span childSpan =
         tracer.spanBuilder("child:1").setParent(Context.current().with(transmitSpans1)).startSpan();
 
-    childSpan.setAttribute(AwsAttributeKeys.AWS_REMOTE_OPERATION, "childOperation");
+    childSpan.setAttribute(testKey2, "testValue3");
 
     Span transmitSpans2 = createNestedSpan(childSpan, 2);
 
-    assertThat(((ReadableSpan) transmitSpans2).getAttribute(AwsAttributeKeys.AWS_REMOTE_OPERATION))
-        .isEqualTo("childOperation");
+    assertThat(((ReadableSpan) transmitSpans2).getAttribute(testKey2)).isEqualTo("testValue3");
   }
 
   @Test
-  public void testRemoteAttributesNotExists() {
+  public void testAttributesDoNotExist() {
     Span span = tracer.spanBuilder("parent").startSpan();
     validateSpanAttributesInheritance(span, null, null, null);
   }
 
   @Test
-  public void testLocalAttributeDetectionBySpanKind() {
+  public void testSpanNamePropagationBySpanKind() {
     for (SpanKind value : SpanKind.values()) {
-      Span span = tracer.spanBuilder("startOperation").setSpanKind(value).startSpan();
+      Span span = tracer.spanBuilder("parent").setSpanKind(value).startSpan();
       if (value == SpanKind.SERVER || value == SpanKind.CONSUMER) {
-        validateSpanAttributesInheritance(span, "startOperation", null, null);
+        validateSpanAttributesInheritance(span, "parent", null, null);
       } else {
         validateSpanAttributesInheritance(span, null, null, null);
       }
@@ -85,7 +93,7 @@ class AwsAttributePropagatingSpanProcessorTest {
   }
 
   @Test
-  public void testLocalAttributeDetectionWithRemoteParentSpan() {
+  public void testSpanNamePropagationWithRemoteParentSpan() {
     Span remoteParent =
         Span.wrap(
             SpanContext.createFromRemoteParent(
@@ -96,11 +104,11 @@ class AwsAttributePropagatingSpanProcessorTest {
     Context parentcontext = Context.root().with(remoteParent);
     Span span =
         tracer
-            .spanBuilder("startOperation")
+            .spanBuilder("parent")
             .setSpanKind(SpanKind.SERVER)
             .setParent(parentcontext)
             .startSpan();
-    validateSpanAttributesInheritance(span, "startOperation", null, null);
+    validateSpanAttributesInheritance(span, "parent", null, null);
   }
 
   private Span createNestedSpan(Span parentSpan, int depth) {
@@ -120,31 +128,25 @@ class AwsAttributePropagatingSpanProcessorTest {
   }
 
   private void validateSpanAttributesInheritance(
-      Span spanWithAppOnly,
-      String localOperation,
-      String remoteApplication,
-      String remoteOperation) {
-    ReadableSpan leafSpan = (ReadableSpan) createNestedSpan(spanWithAppOnly, 10);
+      Span parentSpan, String propagatedName, String propagationValue1, String propagatedValue2) {
+    ReadableSpan leafSpan = (ReadableSpan) createNestedSpan(parentSpan, 10);
 
     assertThat(leafSpan.getParentSpanContext()).isNotNull();
     assertThat(leafSpan.getName()).isEqualTo("child:1");
-    if (localOperation != null) {
-      assertThat(leafSpan.getAttribute(AwsAttributeKeys.AWS_LOCAL_OPERATION))
-          .isEqualTo(localOperation);
+    if (propagatedName != null) {
+      assertThat(leafSpan.getAttribute(spanNameKey)).isEqualTo(propagatedName);
     } else {
-      assertThat(leafSpan.getAttribute(AwsAttributeKeys.AWS_LOCAL_OPERATION)).isNull();
+      assertThat(leafSpan.getAttribute(spanNameKey)).isNull();
     }
-    if (remoteApplication != null) {
-      assertThat(leafSpan.getAttribute(AwsAttributeKeys.AWS_REMOTE_APPLICATION))
-          .isEqualTo(remoteApplication);
+    if (propagationValue1 != null) {
+      assertThat(leafSpan.getAttribute(testKey1)).isEqualTo(propagationValue1);
     } else {
-      assertThat(leafSpan.getAttribute(AwsAttributeKeys.AWS_REMOTE_APPLICATION)).isNull();
+      assertThat(leafSpan.getAttribute(testKey1)).isNull();
     }
-    if (remoteOperation != null) {
-      assertThat(leafSpan.getAttribute(AwsAttributeKeys.AWS_REMOTE_OPERATION))
-          .isEqualTo(remoteOperation);
+    if (propagatedValue2 != null) {
+      assertThat(leafSpan.getAttribute(testKey2)).isEqualTo(propagatedValue2);
     } else {
-      assertThat(leafSpan.getAttribute(AwsAttributeKeys.AWS_REMOTE_OPERATION)).isNull();
+      assertThat(leafSpan.getAttribute(testKey2)).isNull();
     }
   }
 }
