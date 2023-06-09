ee54d77816d5252fa30b95ff88d140e6a939e399
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2023-06-09 13:19:57 +0200

    Adding docs to StorageConfiguration

--- disk-buffering/src/main/java/io/opentelemetry/contrib/disk/buffering/storage/StorageConfiguration.java
+++ disk-buffering/src/main/java/io/opentelemetry/contrib/disk/buffering/storage/StorageConfiguration.java
@@ -3,16 +3,38 @@ package io.opentelemetry.contrib.disk.buffering.storage;
 import com.google.auto.value.AutoValue;
 import java.util.concurrent.TimeUnit;
 
+/** Defines how the storage should be managed. */
 @AutoValue
 public abstract class StorageConfiguration {
+  /**
+   * @return The max amount of time a file can receive new data.
+   */
   public abstract long getMaxFileAgeForWriteMillis();
 
+  /**
+   * @return The min amount of time needed to pass before reading from a file. This value MUST be
+   *     greater than getMaxFileAgeForWriteMillis() to make sure the selected file to read is not
+   *     being written to.
+   */
   public abstract long getMinFileAgeForReadMillis();
 
+  /**
+   * @return The max amount of time a file can be read from, which is also the amount of time a file
+   *     is not considered to be deleted as stale.
+   */
   public abstract long getMaxFileAgeForReadMillis();
 
+  /**
+   * @return The max file size, If the getMaxFileAgeForWriteMillis() time value hasn't passed but
+   *     the file has reached this size, it stops receiving data.
+   */
   public abstract int getMaxFileSize();
 
+  /**
+   * @return All the files are stored in a signal-specific folder. This number represents each
+   *     folder's size, therefore the max amount of cache size for the overall telemetry data would
+   *     be the sum of the folder sizes of all the signals being stored in disk.
+   */
   public abstract int getMaxFolderSize();
 
   public static StorageConfiguration getDefault() {
