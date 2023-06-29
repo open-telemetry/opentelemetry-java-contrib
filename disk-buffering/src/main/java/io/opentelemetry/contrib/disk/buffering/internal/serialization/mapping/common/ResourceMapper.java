5b0b34a4f05a82804c5ad3ed124f52d3dbeca74c
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2023-06-29 12:11:38 +0200

    Removing mapstruct from ResourceMapper

--- disk-buffering/src/main/java/io/opentelemetry/contrib/disk/buffering/internal/serialization/mapping/common/ResourceMapper.java
+++ disk-buffering/src/main/java/io/opentelemetry/contrib/disk/buffering/internal/serialization/mapping/common/ResourceMapper.java
@@ -8,15 +8,10 @@ package io.opentelemetry.contrib.disk.buffering.internal.serialization.mapping.c
 import io.opentelemetry.proto.resource.v1.Resource;
 import io.opentelemetry.sdk.resources.ResourceBuilder;
 import javax.annotation.Nullable;
-import org.mapstruct.AfterMapping;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.ReportingPolicy;
 
-@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE)
-public abstract class ResourceMapper {
+public final class ResourceMapper {
 
-  public static final ResourceMapper INSTANCE = new ResourceMapperImpl();
+  public static final ResourceMapper INSTANCE = new ResourceMapper();
 
   public Resource mapToProto(io.opentelemetry.sdk.resources.Resource sdkResource) {
     return Resource.newBuilder()
@@ -24,11 +19,14 @@ public abstract class ResourceMapper {
         .build();
   }
 
-  public abstract io.opentelemetry.sdk.resources.Resource mapToSdk(
-      Resource protoResource, @Nullable String schemaUrl);
+  public io.opentelemetry.sdk.resources.Resource mapToSdk(
+      Resource protoResource, @Nullable String schemaUrl) {
+    ResourceBuilder resource = io.opentelemetry.sdk.resources.Resource.builder();
 
-  @AfterMapping
-  protected void addAttributes(Resource protoResource, @MappingTarget ResourceBuilder builder) {
-    builder.putAll(AttributesMapper.INSTANCE.protoToAttributes(protoResource.getAttributesList()));
+    if (schemaUrl != null) {
+      resource.setSchemaUrl(schemaUrl);
+    }
+    resource.putAll(AttributesMapper.INSTANCE.protoToAttributes(protoResource.getAttributesList()));
+    return resource.build();
   }
 }
