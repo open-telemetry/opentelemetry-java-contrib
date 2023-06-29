2b05d9688e69ffe4ec1b371bf9be440ce8e4ac0d
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2023-06-29 12:12:13 +0200

    Removing mapstruct from the dependencies

--- disk-buffering/build.gradle.kts
+++ disk-buffering/build.gradle.kts
@@ -12,16 +12,13 @@ java {
   targetCompatibility = JavaVersion.VERSION_1_8
 }
 
-val mapStructVersion = "1.5.5.Final"
 val autovalueVersion = "1.10.1"
 dependencies {
   api("io.opentelemetry:opentelemetry-sdk")
   implementation("io.opentelemetry:opentelemetry-exporter-otlp-common")
   implementation("io.opentelemetry.proto:opentelemetry-proto:0.20.0-alpha")
-  implementation("org.mapstruct:mapstruct:$mapStructVersion")
   compileOnly("com.google.auto.value:auto-value-annotations:$autovalueVersion")
   annotationProcessor("com.google.auto.value:auto-value:$autovalueVersion")
-  annotationProcessor("org.mapstruct:mapstruct-processor:$mapStructVersion")
   testImplementation("org.mockito:mockito-inline:4.11.0")
   testImplementation("io.opentelemetry:opentelemetry-sdk-testing")
 }
