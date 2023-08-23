7f9a7651b75d33afe7c5f52421db0eb0af727529
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2023-08-23 14:13:15 +0200

    Creating processors subproject

---
+++ processors/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+  id("otel.java-conventions")
+  id("otel.publish-conventions")
+}
+
+description = "Tools to intercept and process signals globally."
+otelJava.moduleName.set("io.opentelemetry.contrib.processors")
+
+java {
+  sourceCompatibility = JavaVersion.VERSION_1_8
+  targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+dependencies {
+  api("io.opentelemetry:opentelemetry-sdk")
+}
