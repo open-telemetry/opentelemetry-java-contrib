1faffe9c301ca40f217c99d1537b37b41215041c
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2023-08-25 10:15:56 +0200

    Using CopyOnWriteArrayList in ComposableInterceptor

--- processors/src/main/java/io/opentelemetry/contrib/interceptor/common/ComposableInterceptor.java
+++ processors/src/main/java/io/opentelemetry/contrib/interceptor/common/ComposableInterceptor.java
@@ -6,17 +6,18 @@
 package io.opentelemetry.contrib.interceptor.common;
 
 import io.opentelemetry.contrib.interceptor.api.Interceptor;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import javax.annotation.Nullable;
 
 /** Allows to run an item through a list of interceptors in the order they were added. */
 public final class ComposableInterceptor<T> implements Interceptor<T> {
-  private final List<Interceptor<T>> interceptors = new CopyOnWriteArrayList<>();
+  private final CopyOnWriteArrayList<Interceptor<T>> interceptors = new CopyOnWriteArrayList<>();
 
   public void add(Interceptor<T> interceptor) {
     interceptors.addIfAbsent(interceptor);
   }
 
+  @Nullable
   @Override
   public T intercept(T item) {
     T intercepted = item;
