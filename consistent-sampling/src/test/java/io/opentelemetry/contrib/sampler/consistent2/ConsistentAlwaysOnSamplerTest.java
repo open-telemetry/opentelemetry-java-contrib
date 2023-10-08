4bd4820be7a0e0aea3514b08a516900b39f81f59
Otmar Ertl <otmar.ertl@dynatrace.com>
2023-10-08 16:42:24 +0200

    ConsistentAlwaysOnSamplerTest

---
+++ consistent-sampling/src/test/java/io/opentelemetry/contrib/sampler/consistent2/ConsistentAlwaysOnSamplerTest.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.contrib.sampler.consistent2;
+
+import static io.opentelemetry.contrib.sampler.consistent2.ConsistentSamplingUtil.getInvalidThreshold;
+import static io.opentelemetry.contrib.sampler.consistent2.ConsistentSamplingUtil.getMaxThreshold;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class ConsistentAlwaysOnSamplerTest {
+
+  @Test
+  void testDescription() {
+    assertThat(ConsistentSampler.alwaysOn().getDescription())
+        .isEqualTo("ConsistentAlwaysOnSampler");
+  }
+
+  @Test
+  void testThreshold() {
+    assertThat(ConsistentSampler.alwaysOn().getThreshold(getInvalidThreshold(), false))
+        .isEqualTo(getMaxThreshold());
+    assertThat(ConsistentSampler.alwaysOn().getThreshold(getInvalidThreshold(), true))
+        .isEqualTo(getMaxThreshold());
+    assertThat(ConsistentSampler.alwaysOn().getThreshold(getMaxThreshold(), false))
+        .isEqualTo(getMaxThreshold());
+    assertThat(ConsistentSampler.alwaysOn().getThreshold(getMaxThreshold(), true))
+        .isEqualTo(getMaxThreshold());
+    assertThat(ConsistentSampler.alwaysOn().getThreshold(0, false)).isEqualTo(getMaxThreshold());
+    assertThat(ConsistentSampler.alwaysOn().getThreshold(0, true)).isEqualTo(getMaxThreshold());
+  }
+}
