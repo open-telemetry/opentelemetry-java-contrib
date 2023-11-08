2983ea70e9c794c4ae44827cc7dfa4cfddeb501f
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2023-11-08 18:09:41 +0100

    add extended tracer module (#759)
    Co-authored-by: Trask Stalnaker <trask.stalnaker@gmail.com>

---
+++ extended-tracer/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+  id("otel.java-conventions")
+}
+
+description = "Extended Tracer"
+otelJava.moduleName.set("io.opentelemetry.contrib.extended-tracer")
+
+dependencies {
+  api("io.opentelemetry:opentelemetry-api")
+  testImplementation("io.opentelemetry:opentelemetry-sdk-testing")
+  testImplementation("io.opentelemetry.semconv:opentelemetry-semconv")
+}
