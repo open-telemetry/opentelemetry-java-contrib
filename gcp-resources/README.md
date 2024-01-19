33feb349b0f98c2112f1b1819e337869fd21a13e
Pranav Sharma <sharmapranav@google.com>
2024-01-19 22:23:11 +0000

    Add GCP resource detection module

---
+++ gcp-resources/README.md
@@ -0,0 +1,58 @@
+# GCP Resource Detectors for OpenTelemetry
+
+This module provides GCP resource detectors for OpenTelemetry.
+
+The following OpenTelemetry semantic conventions will be detected:
+
+| Resource attribute | GCE | GKE | GCR | GCF | GAE |
+| ------------------ | --- | --- | --- | --- | --- |
+| cloud.platform | gcp_compute_engine | gcp_kubernetes_engine | gcp_cloud_run | gcp_cloud_run | gcp_app_engine |
+| cloud.provider | gcp | gcp | gcp | gcp | gcp |
+| cloud.account.id | auto | auto | auto | auto | auto |
+| cloud.availability_zone | auto | auto | auto | auto | auto |
+| cloud.region | auto | auto | auto | auto | auto |
+| host.id | auto | auto | | | |
+| host.name | auto | auto | | | |
+| host.type | auto | auto | | | |
+| k8s.pod.name | | downward API or auto | | | |
+| k8s.namespace.name | | downward API | | | |
+| k8s.container.name | | hardcoded | | | |
+| k8s.cluster.name | | auto | | | |
+| faas.name | | | auto | auto | auto |
+| faas.version | | | auto | auto | auto |
+| faas.instance | | | auto | auto | auto |
+
+
+## Downward API
+
+For GKE applications, some values most be passed via the environment variable using k8s
+"downward API".  For example, the following spec will ensure `k8s.namespace.name` and
+`k8s.pod.name` are correctly discovered:
+
+```yaml
+spec:
+  containers:
+    - name: my-application
+      image: gcr.io/my-project/my-image:latest
+      env:
+        - name: POD_NAME
+          valueFrom:
+            fieldRef:
+              fieldPath: metadata.name
+        - name: NAMESPACE
+          valueFrom:
+            fieldRef:
+              fieldPath: metadata.namespace
+        - name: CONTAINER_NAME
+          value: my-application
+```
+
+Additionally, the container name will only be discovered via the environment variable `CONTAINER_NAME`
+which much be included in the environment.
+
+## Component Owners
+
+- [Josh Suereth](https://github.com/jsuereth), Google
+- [Pranav Sharma](https://github.com/psx95), Google
+
+Learn more about component owners in [component_owners.yml](../.github/component_owners.yml).
---
+++ gcp-resources/src/main/java/io/opentelemetry/contrib/gcp/resource/GCPResourceProvider.java
@@ -0,0 +1,253 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.contrib.gcp.resource;
+
+import static com.google.cloud.opentelemetry.detection.AttributeKeys.GAE_APP_VERSION;
+import static com.google.cloud.opentelemetry.detection.AttributeKeys.GAE_AVAILABILITY_ZONE;
+import static com.google.cloud.opentelemetry.detection.AttributeKeys.GAE_CLOUD_REGION;
+import static com.google.cloud.opentelemetry.detection.AttributeKeys.GAE_INSTANCE_ID;
+import static com.google.cloud.opentelemetry.detection.AttributeKeys.GAE_MODULE_NAME;
+import static com.google.cloud.opentelemetry.detection.AttributeKeys.GCE_AVAILABILITY_ZONE;
+import static com.google.cloud.opentelemetry.detection.AttributeKeys.GCE_CLOUD_REGION;
+import static com.google.cloud.opentelemetry.detection.AttributeKeys.GCE_INSTANCE_HOSTNAME;
+import static com.google.cloud.opentelemetry.detection.AttributeKeys.GCE_INSTANCE_ID;
+import static com.google.cloud.opentelemetry.detection.AttributeKeys.GCE_INSTANCE_NAME;
+import static com.google.cloud.opentelemetry.detection.AttributeKeys.GCE_MACHINE_TYPE;
+import static com.google.cloud.opentelemetry.detection.AttributeKeys.GKE_CLUSTER_LOCATION;
+import static com.google.cloud.opentelemetry.detection.AttributeKeys.GKE_CLUSTER_LOCATION_TYPE;
+import static com.google.cloud.opentelemetry.detection.AttributeKeys.GKE_CLUSTER_NAME;
+import static com.google.cloud.opentelemetry.detection.AttributeKeys.GKE_HOST_ID;
+import static com.google.cloud.opentelemetry.detection.AttributeKeys.GKE_LOCATION_TYPE_REGION;
+import static com.google.cloud.opentelemetry.detection.AttributeKeys.GKE_LOCATION_TYPE_ZONE;
+import static com.google.cloud.opentelemetry.detection.AttributeKeys.SERVERLESS_COMPUTE_AVAILABILITY_ZONE;
+import static com.google.cloud.opentelemetry.detection.AttributeKeys.SERVERLESS_COMPUTE_CLOUD_REGION;
+import static com.google.cloud.opentelemetry.detection.AttributeKeys.SERVERLESS_COMPUTE_INSTANCE_ID;
+import static com.google.cloud.opentelemetry.detection.AttributeKeys.SERVERLESS_COMPUTE_NAME;
+import static com.google.cloud.opentelemetry.detection.AttributeKeys.SERVERLESS_COMPUTE_REVISION;
+
+import com.google.cloud.opentelemetry.detection.DetectedPlatform;
+import com.google.cloud.opentelemetry.detection.GCPPlatformDetector;
+import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.api.common.AttributesBuilder;
+import io.opentelemetry.sdk.autoconfigure.spi.ConfigProperties;
+import io.opentelemetry.sdk.autoconfigure.spi.ResourceProvider;
+import io.opentelemetry.sdk.resources.Resource;
+import io.opentelemetry.semconv.ResourceAttributes;
+import java.util.Map;
+import java.util.Optional;
+import java.util.logging.Logger;
+
+public class GCPResourceProvider implements ResourceProvider {
+
+  private static final Logger LOGGER = Logger.getLogger(GCPResourceProvider.class.getSimpleName());
+  private final GCPPlatformDetector detector;
+
+  // for testing only
+  GCPResourceProvider(GCPPlatformDetector detector) {
+    this.detector = detector;
+  }
+
+  public GCPResourceProvider() {
+    this.detector = GCPPlatformDetector.DEFAULT_INSTANCE;
+  }
+
+  /**
+   * Generates and returns the attributes for the resource. The attributes vary depending on the
+   * type of resource detected.
+   *
+   * @return The {@link Attributes} for the detected resource.
+   */
+  public Attributes getAttributes() {
+    DetectedPlatform detectedPlatform = detector.detectPlatform();
+    if (detectedPlatform.getSupportedPlatform()
+        == GCPPlatformDetector.SupportedPlatform.UNKNOWN_PLATFORM) {
+      return Attributes.empty();
+    }
+
+    // This is running on some sort of GCPCompute - figure out the platform
+    AttributesBuilder attrBuilder = Attributes.builder();
+    attrBuilder.put(ResourceAttributes.CLOUD_PROVIDER, ResourceAttributes.CloudProviderValues.GCP);
+    attrBuilder.put(ResourceAttributes.CLOUD_ACCOUNT_ID, detectedPlatform.getProjectId());
+
+    switch (detectedPlatform.getSupportedPlatform()) {
+      case GOOGLE_KUBERNETES_ENGINE:
+        addGkeAttributes(attrBuilder, detectedPlatform.getAttributes());
+        break;
+      case GOOGLE_CLOUD_RUN:
+        addGcrAttributes(attrBuilder, detectedPlatform.getAttributes());
+        break;
+      case GOOGLE_CLOUD_FUNCTIONS:
+        addGcfAttributes(attrBuilder, detectedPlatform.getAttributes());
+        break;
+      case GOOGLE_APP_ENGINE:
+        addGaeAttributes(attrBuilder, detectedPlatform.getAttributes());
+        break;
+      case GOOGLE_COMPUTE_ENGINE:
+        addGceAttributes(attrBuilder, detectedPlatform.getAttributes());
+        break;
+      default:
+        // We don't support this platform yet, so just return with what we have
+    }
+
+    return attrBuilder.build();
+  }
+
+  @Override
+  public Resource createResource(ConfigProperties config) {
+    return Resource.create(getAttributes());
+  }
+
+  /**
+   * Updates the attributes builder with required attributes for GCE resource, if GCE resource is
+   * applicable. By default, if the resource is running on GCP, it is assumed to be GCE. This means
+   * additional attributes are added/overwritten if later on, the resource is identified to be some
+   * other platform - like GKE, GAE, etc.
+   */
+  private static void addGceAttributes(
+      AttributesBuilder attrBuilder, Map<String, String> attributesMap) {
+    attrBuilder.put(
+        ResourceAttributes.CLOUD_PLATFORM,
+        ResourceAttributes.CloudPlatformValues.GCP_COMPUTE_ENGINE);
+
+    Optional.ofNullable(attributesMap.get(GCE_AVAILABILITY_ZONE))
+        .ifPresent(zone -> attrBuilder.put(ResourceAttributes.CLOUD_AVAILABILITY_ZONE, zone));
+    Optional.ofNullable(attributesMap.get(GCE_CLOUD_REGION))
+        .ifPresent(region -> attrBuilder.put(ResourceAttributes.CLOUD_REGION, region));
+    Optional.ofNullable(attributesMap.get(GCE_INSTANCE_ID))
+        .ifPresent(instanceId -> attrBuilder.put(ResourceAttributes.HOST_ID, instanceId));
+    Optional.ofNullable(attributesMap.get(GCE_INSTANCE_NAME))
+        .ifPresent(
+            instanceName -> {
+              attrBuilder.put(ResourceAttributes.HOST_NAME, instanceName);
+              attrBuilder.put(ResourceAttributes.GCP_GCE_INSTANCE_NAME, instanceName);
+            });
+    Optional.ofNullable(attributesMap.get(GCE_INSTANCE_HOSTNAME))
+        .ifPresent(
+            instanceHostname ->
+                attrBuilder.put(ResourceAttributes.GCP_GCE_INSTANCE_HOSTNAME, instanceHostname));
+    Optional.ofNullable(attributesMap.get(GCE_MACHINE_TYPE))
+        .ifPresent(machineType -> attrBuilder.put(ResourceAttributes.HOST_TYPE, machineType));
+  }
+
+  /**
+   * Updates the attributes with the required keys for a GKE (Google Kubernetes Engine) environment.
+   * The attributes are not updated in case the environment is not deemed to be GKE.
+   *
+   * @param attrBuilder The {@link AttributesBuilder} object that needs to be updated with the
+   *     necessary keys.
+   */
+  private static void addGkeAttributes(
+      AttributesBuilder attrBuilder, Map<String, String> attributesMap) {
+    attrBuilder.put(
+        ResourceAttributes.CLOUD_PLATFORM,
+        ResourceAttributes.CloudPlatformValues.GCP_KUBERNETES_ENGINE);
+
+    Optional.ofNullable(attributesMap.get(GKE_CLUSTER_NAME))
+        .ifPresent(
+            clusterName -> attrBuilder.put(ResourceAttributes.K8S_CLUSTER_NAME, clusterName));
+    Optional.ofNullable(attributesMap.get(GKE_HOST_ID))
+        .ifPresent(hostId -> attrBuilder.put(ResourceAttributes.HOST_ID, hostId));
+    Optional.ofNullable(attributesMap.get(GKE_CLUSTER_LOCATION_TYPE))
+        .ifPresent(
+            locationType -> {
+              if (attributesMap.get(GKE_CLUSTER_LOCATION) != null) {
+                switch (locationType) {
+                  case GKE_LOCATION_TYPE_REGION:
+                    attrBuilder.put(
+                        ResourceAttributes.CLOUD_REGION, attributesMap.get(GKE_CLUSTER_LOCATION));
+                    break;
+                  case GKE_LOCATION_TYPE_ZONE:
+                    attrBuilder.put(
+                        ResourceAttributes.CLOUD_AVAILABILITY_ZONE,
+                        attributesMap.get(GKE_CLUSTER_LOCATION));
+                    break;
+                  default:
+                    // TODO: Figure out how to handle unexpected conditions like this - Issue #183
+                    LOGGER.severe(
+                        String.format(
+                            "Unrecognized format for cluster location: %s",
+                            attributesMap.get(GKE_CLUSTER_LOCATION)));
+                }
+              }
+            });
+  }
+
+  /**
+   * Updates the attributes with the required keys for a GCR (Google Cloud Run) environment. The
+   * attributes are not updated in case the environment is not deemed to be GCR.
+   *
+   * @param attrBuilder The {@link AttributesBuilder} object that needs to be updated with the
+   *     necessary keys.
+   */
+  private static void addGcrAttributes(
+      AttributesBuilder attrBuilder, Map<String, String> attributesMap) {
+    attrBuilder.put(
+        ResourceAttributes.CLOUD_PLATFORM, ResourceAttributes.CloudPlatformValues.GCP_CLOUD_RUN);
+    addCommonAttributesForServerlessCompute(attrBuilder, attributesMap);
+  }
+
+  /**
+   * Updates the attributes with the required keys for a GCF (Google Cloud Functions) environment.
+   * The attributes are not updated in case the environment is not deemed to be GCF.
+   *
+   * @param attrBuilder The {@link AttributesBuilder} object that needs to be updated with the
+   *     necessary keys.
+   */
+  private static void addGcfAttributes(
+      AttributesBuilder attrBuilder, Map<String, String> attributesMap) {
+    attrBuilder.put(
+        ResourceAttributes.CLOUD_PLATFORM,
+        ResourceAttributes.CloudPlatformValues.GCP_CLOUD_FUNCTIONS);
+    addCommonAttributesForServerlessCompute(attrBuilder, attributesMap);
+  }
+
+  /**
+   * Updates the attributes with the required keys for a GAE (Google App Engine) environment. The
+   * attributes are not updated in case the environment is not deemed to be GAE.
+   *
+   * @param attrBuilder The {@link AttributesBuilder} object that needs to be updated with the
+   *     necessary keys.
+   */
+  private static void addGaeAttributes(
+      AttributesBuilder attrBuilder, Map<String, String> attributesMap) {
+    attrBuilder.put(
+        ResourceAttributes.CLOUD_PLATFORM, ResourceAttributes.CloudPlatformValues.GCP_APP_ENGINE);
+    Optional.ofNullable(attributesMap.get(GAE_MODULE_NAME))
+        .ifPresent(appName -> attrBuilder.put(ResourceAttributes.FAAS_NAME, appName));
+    Optional.ofNullable(attributesMap.get(GAE_APP_VERSION))
+        .ifPresent(appVersion -> attrBuilder.put(ResourceAttributes.FAAS_VERSION, appVersion));
+    Optional.ofNullable(attributesMap.get(GAE_INSTANCE_ID))
+        .ifPresent(
+            appInstanceId -> attrBuilder.put(ResourceAttributes.FAAS_INSTANCE, appInstanceId));
+    Optional.ofNullable(attributesMap.get(GAE_CLOUD_REGION))
+        .ifPresent(cloudRegion -> attrBuilder.put(ResourceAttributes.CLOUD_REGION, cloudRegion));
+    Optional.ofNullable(attributesMap.get(GAE_AVAILABILITY_ZONE))
+        .ifPresent(
+            cloudAvailabilityZone ->
+                attrBuilder.put(ResourceAttributes.CLOUD_AVAILABILITY_ZONE, cloudAvailabilityZone));
+  }
+
+  /**
+   * This function adds common attributes required for most serverless compute platforms within GCP.
+   * Currently, these attributes are required for both GCF and GCR.
+   *
+   * @param attrBuilder The {@link AttributesBuilder} object that needs to be updated with the
+   *     necessary keys.
+   */
+  private static void addCommonAttributesForServerlessCompute(
+      AttributesBuilder attrBuilder, Map<String, String> attributesMap) {
+    Optional.ofNullable(attributesMap.get(SERVERLESS_COMPUTE_NAME))
+        .ifPresent(name -> attrBuilder.put(ResourceAttributes.FAAS_NAME, name));
+    Optional.ofNullable(attributesMap.get(SERVERLESS_COMPUTE_REVISION))
+        .ifPresent(revision -> attrBuilder.put(ResourceAttributes.FAAS_VERSION, revision));
+    Optional.ofNullable(attributesMap.get(SERVERLESS_COMPUTE_INSTANCE_ID))
+        .ifPresent(instanceId -> attrBuilder.put(ResourceAttributes.FAAS_INSTANCE, instanceId));
+    Optional.ofNullable(attributesMap.get(SERVERLESS_COMPUTE_AVAILABILITY_ZONE))
+        .ifPresent(zone -> attrBuilder.put(ResourceAttributes.CLOUD_AVAILABILITY_ZONE, zone));
+    Optional.ofNullable(attributesMap.get(SERVERLESS_COMPUTE_CLOUD_REGION))
+        .ifPresent(region -> attrBuilder.put(ResourceAttributes.CLOUD_REGION, region));
+  }
+}
