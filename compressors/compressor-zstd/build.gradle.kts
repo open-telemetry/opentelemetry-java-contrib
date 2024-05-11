a07677d19f2cd564ad6ae0e34cc3e0fba8a782ad
Trask Stalnaker <trask.stalnaker@gmail.com>
2024-05-11 01:45:26 -0700

    Fix todo (#1313)

--- compressors/compressor-zstd/build.gradle.kts
+++ compressors/compressor-zstd/build.gradle.kts
@@ -7,14 +7,12 @@ description = "zstd compressor implementation for use with OTLP exporters"
 otelJava.moduleName.set("io.opentelemetry.contrib.compressor.zstd")
 
 dependencies {
-  // TODO(jack-berg): Use version from :depedencyManagement when opentelemetry-instrumentation-bom-alpha depends on opentelemetry-java 1.34.0
-  var openTelemetryVersion = "1.37.0"
-  api("io.opentelemetry:opentelemetry-exporter-common:$openTelemetryVersion")
+  api("io.opentelemetry:opentelemetry-exporter-common")
 
   implementation("com.github.luben:zstd-jni:1.5.6-3")
 
-  testImplementation("io.opentelemetry:opentelemetry-sdk-testing:$openTelemetryVersion")
-  testImplementation("io.opentelemetry:opentelemetry-exporter-otlp:$openTelemetryVersion")
+  testImplementation("io.opentelemetry:opentelemetry-sdk-testing")
+  testImplementation("io.opentelemetry:opentelemetry-exporter-otlp")
 
   testImplementation("io.opentelemetry.proto:opentelemetry-proto")
   testImplementation("com.linecorp.armeria:armeria-junit5")
