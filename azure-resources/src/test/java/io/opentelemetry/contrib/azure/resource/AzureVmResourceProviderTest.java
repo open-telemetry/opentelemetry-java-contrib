11cfead04ca1bf40b4f0bdf983d5eb3e7cb909b3
heyams <heya@microsoft.com>
2024-06-05 12:15:05 -0700

    Fix tests

--- azure-resources/src/test/java/io/opentelemetry/contrib/azure/resource/AzureVmResourceProviderTest.java
+++ azure-resources/src/test/java/io/opentelemetry/contrib/azure/resource/AzureVmResourceProviderTest.java
@@ -5,6 +5,16 @@
 
 package io.opentelemetry.contrib.azure.resource;
 
+import static io.opentelemetry.semconv.incubating.CloudIncubatingAttributes.CLOUD_PLATFORM;
+import static io.opentelemetry.semconv.incubating.CloudIncubatingAttributes.CLOUD_PROVIDER;
+import static io.opentelemetry.semconv.incubating.CloudIncubatingAttributes.CLOUD_REGION;
+import static io.opentelemetry.semconv.incubating.CloudIncubatingAttributes.CLOUD_RESOURCE_ID;
+import static io.opentelemetry.semconv.incubating.HostIncubatingAttributes.HOST_ID;
+import static io.opentelemetry.semconv.incubating.HostIncubatingAttributes.HOST_NAME;
+import static io.opentelemetry.semconv.incubating.HostIncubatingAttributes.HOST_TYPE;
+import static io.opentelemetry.semconv.incubating.OsIncubatingAttributes.OS_TYPE;
+import static org.mockito.internal.util.Platform.OS_VERSION;
+
 import com.google.common.base.Charsets;
 import com.google.common.io.CharStreams;
 import com.linecorp.armeria.common.HttpResponse;
@@ -14,7 +24,7 @@ import com.linecorp.armeria.testing.junit5.server.mock.MockWebServerExtension;
 import io.opentelemetry.sdk.resources.Resource;
 import io.opentelemetry.sdk.testing.assertj.AttributesAssert;
 import io.opentelemetry.sdk.testing.assertj.OpenTelemetryAssertions;
-import io.opentelemetry.semconv.ResourceAttributes;
+import io.opentelemetry.semconv.incubating.CloudIncubatingAttributes;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
@@ -80,25 +90,23 @@ class AzureVmResourceProviderTest {
   private static void assertOnlyProvider(AttributesAssert attributesAssert) {
     attributesAssert
         .hasSize(2)
-        .containsEntry(ResourceAttributes.CLOUD_PROVIDER, "azure")
-        .containsEntry(
-            ResourceAttributes.CLOUD_PLATFORM, ResourceAttributes.CloudPlatformValues.AZURE_VM);
+        .containsEntry(CLOUD_PROVIDER, "azure")
+        .containsEntry(CLOUD_PLATFORM, CloudIncubatingAttributes.CloudPlatformValues.AZURE_VM);
   }
 
   private static void assertDefaultAttributes(AttributesAssert attributesAssert) {
     attributesAssert
-        .containsEntry(ResourceAttributes.CLOUD_PROVIDER, "azure")
+        .containsEntry(CLOUD_PROVIDER, "azure")
+        .containsEntry(CLOUD_PLATFORM, CloudIncubatingAttributes.CloudPlatformValues.AZURE_VM)
+        .containsEntry(CLOUD_REGION, "westus")
         .containsEntry(
-            ResourceAttributes.CLOUD_PLATFORM, ResourceAttributes.CloudPlatformValues.AZURE_VM)
-        .containsEntry(ResourceAttributes.CLOUD_REGION, "westus")
-        .containsEntry(
-            ResourceAttributes.CLOUD_RESOURCE_ID,
+            CLOUD_RESOURCE_ID,
             "/subscriptions/xxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxx/resourceGroups/macikgo-test-may-23/providers/Microsoft.Compute/virtualMachines/examplevmname")
-        .containsEntry(ResourceAttributes.HOST_ID, "02aab8a4-74ef-476e-8182-f6d2ba4166a6")
-        .containsEntry(ResourceAttributes.HOST_NAME, "examplevmname")
-        .containsEntry(ResourceAttributes.HOST_TYPE, "Standard_A3")
-        .containsEntry(ResourceAttributes.OS_TYPE, "Linux")
-        .containsEntry(ResourceAttributes.OS_VERSION, "15.05.22")
+        .containsEntry(HOST_ID, "02aab8a4-74ef-476e-8182-f6d2ba4166a6")
+        .containsEntry(HOST_NAME, "examplevmname")
+        .containsEntry(HOST_TYPE, "Standard_A3")
+        .containsEntry(OS_TYPE, "Linux")
+        .containsEntry(OS_VERSION, "15.05.22")
         .containsEntry("azure.vm.scaleset.name", "crpteste9vflji9")
         .containsEntry("azure.vm.sku", "18.04-LTS");
   }
