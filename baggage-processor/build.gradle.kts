d23bc116c0bb6e9bfff2850ca8cafd0bb432a03e
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-06-17 21:46:02 +0200

    add config support for BaggageSpanProcessor (#1330)
    Co-authored-by: Mike Goldsmith <goldsmith.mike@gmail.com>

--- baggage-processor/build.gradle.kts
+++ baggage-processor/build.gradle.kts
@@ -10,6 +10,11 @@ otelJava.moduleName.set("io.opentelemetry.contrib.baggage.processor")
 dependencies {
   api("io.opentelemetry:opentelemetry-api")
   api("io.opentelemetry:opentelemetry-sdk")
+  implementation("io.opentelemetry:opentelemetry-sdk-extension-autoconfigure-spi")
 
   testImplementation("io.opentelemetry:opentelemetry-sdk-extension-autoconfigure")
+  testImplementation("io.opentelemetry:opentelemetry-sdk-testing")
+  testImplementation("org.mockito:mockito-inline")
+  testImplementation("com.google.guava:guava")
+  testImplementation("org.awaitility:awaitility")
 }
