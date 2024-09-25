140ac1708bc2185c0262c64ca3912d6f0e2cce12
Sylvain Juge <763082+SylvainJuge@users.noreply.github.com>
2024-09-25 11:16:07 +0200

    restore missing serialization ids

--- jmx-scraper/src/main/java/io/opentelemetry/contrib/jmxscraper/ArgumentsParsingException.java
+++ jmx-scraper/src/main/java/io/opentelemetry/contrib/jmxscraper/ArgumentsParsingException.java
@@ -6,6 +6,7 @@
 package io.opentelemetry.contrib.jmxscraper;
 
 public class ArgumentsParsingException extends Exception {
+  private static final long serialVersionUID = 0L;
 
   public ArgumentsParsingException(String msg) {
     super(msg);
--- jmx-scraper/src/main/java/io/opentelemetry/contrib/jmxscraper/config/ConfigurationException.java
+++ jmx-scraper/src/main/java/io/opentelemetry/contrib/jmxscraper/config/ConfigurationException.java
@@ -6,6 +6,7 @@
 package io.opentelemetry.contrib.jmxscraper.config;
 
 public class ConfigurationException extends Exception {
+  private static final long serialVersionUID = 0L;
 
   public ConfigurationException(String message, Throwable cause) {
     super(message, cause);
