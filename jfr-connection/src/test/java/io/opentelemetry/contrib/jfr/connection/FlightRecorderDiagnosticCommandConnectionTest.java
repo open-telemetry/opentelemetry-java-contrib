1facf59d329bd7ee680e9edd144c9f8d6d122a0c
David Grieve <dagrieve@microsoft.com>
2024-10-15 14:33:17 -0600

    add end-to-end diagnostic command test

--- jfr-connection/src/test/java/io/opentelemetry/contrib/jfr/connection/FlightRecorderDiagnosticCommandConnectionTest.java
+++ jfr-connection/src/test/java/io/opentelemetry/contrib/jfr/connection/FlightRecorderDiagnosticCommandConnectionTest.java
@@ -7,12 +7,15 @@ package io.opentelemetry.contrib.jfr.connection;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import java.lang.management.ManagementFactory;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import javax.management.MBeanServer;
 import javax.management.MBeanServerConnection;
 import javax.management.ObjectName;
@@ -71,6 +74,36 @@ class FlightRecorderDiagnosticCommandConnectionTest {
         connection.startRecording(
             new RecordingOptions.Builder().build(), RecordingConfiguration.PROFILE_CONFIGURATION);
     assertEquals(id, 99);
+  }
+
+  @Test
+  void endToEndTest() throws Exception {
+
+    MBeanServerConnection mBeanServer = ManagementFactory.getPlatformMBeanServer();
+    FlightRecorderConnection flightRecorderConnection =
+        FlightRecorderDiagnosticCommandConnection.connect(mBeanServer);
+    RecordingOptions recordingOptions =
+        new RecordingOptions.Builder().disk("true").duration("5s").build();
+    RecordingConfiguration recordingConfiguration = RecordingConfiguration.PROFILE_CONFIGURATION;
+    Path tempFile = Files.createTempFile("recording", ".jfr");
+
+    try (Recording recording =
+        flightRecorderConnection.newRecording(recordingOptions, recordingConfiguration)) {
+
+      recording.start();
+      try {
+        Thread.sleep(1000);
+      } catch (InterruptedException e) {
+        Thread.currentThread().interrupt();
+      }
+      recording.dump(tempFile.toString());
+      recording.stop();
+    } finally {
+      if (!Files.exists(tempFile)) {
+        fail("Recording file not found");
+      }
+      Files.deleteIfExists(tempFile);
+    }
   }
 
   MBeanServerConnection mockMbeanServer(
