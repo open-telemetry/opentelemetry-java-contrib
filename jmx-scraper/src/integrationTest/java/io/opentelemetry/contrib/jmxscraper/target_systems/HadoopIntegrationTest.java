10393a208e9cc4781d3835ed6b1f4c86e323a0fa
Magda Wojtowicz <mwojtowicz@splunk.com>
2024-10-31 18:59:49 +0100

    troubleshooting

--- jmx-scraper/src/integrationTest/java/io/opentelemetry/contrib/jmxscraper/target_systems/HadoopIntegrationTest.java
+++ jmx-scraper/src/integrationTest/java/io/opentelemetry/contrib/jmxscraper/target_systems/HadoopIntegrationTest.java
@@ -24,7 +24,9 @@ public class HadoopIntegrationTest extends TargetSystemIntegrationTest {
                 + " -Dcom.sun.management.jmxremote.ssl=false"
                 + " -Dcom.sun.management.jmxremote.authenticate=false"
                 + " -Dcom.sun.management.jmxremote.local.only=false")
-        .withStartupTimeout(Duration.ofMinutes(2))
+        .withStartupTimeout(Duration.ofMinutes(20))
+//        .withExposedPorts(jmxPort, 10020, 19888, 50010, 50020, 50070, 50075, 50090, 8020, 8042, 8088, 9000)
+        .withExposedPorts(jmxPort, 8004)
         .withCreateContainerCmdModifier(cmd -> cmd.withHostName("test-host"))
         .waitingFor(Wait.forListeningPort());
   }
@@ -36,6 +38,15 @@ public class HadoopIntegrationTest extends TargetSystemIntegrationTest {
 
   @Override
   protected void verifyMetrics() {
+//    waitAndAssertMetrics(
+//        metric ->
+//            assertSumWithAttributes(
+//                metric,
+//                "hadoop.name_node.capacity.usage",
+//                "The current used capacity across all data nodes reporting to the name node.",
+//                "by",
+//                attrs -> attrs.contains(entry("node_name", "test-host")))
+//    );
     //    waitAndAssertMetrics(
     //        metric ->
     //            assertGauge(
