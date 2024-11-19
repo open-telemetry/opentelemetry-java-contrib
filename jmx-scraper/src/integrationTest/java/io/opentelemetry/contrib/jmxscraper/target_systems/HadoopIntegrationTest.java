ae661ae1cc683a9fbd0309259e900181727e60c2
Magda Wojtowicz <mwojtowicz@splunk.com>
2024-11-19 19:40:51 +0100

    troubleshooting

--- jmx-scraper/src/integrationTest/java/io/opentelemetry/contrib/jmxscraper/target_systems/HadoopIntegrationTest.java
+++ jmx-scraper/src/integrationTest/java/io/opentelemetry/contrib/jmxscraper/target_systems/HadoopIntegrationTest.java
@@ -5,28 +5,25 @@
 
 package io.opentelemetry.contrib.jmxscraper.target_systems;
 
+import static io.opentelemetry.contrib.jmxscraper.target_systems.MetricAssertions.assertSumWithAttributes;
+import static org.assertj.core.api.Assertions.entry;
+
 import io.opentelemetry.contrib.jmxscraper.JmxScraperContainer;
 import java.time.Duration;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.utility.MountableFile;
 
 public class HadoopIntegrationTest extends TargetSystemIntegrationTest {
 
   @Override
   protected GenericContainer<?> createTargetContainer(int jmxPort) {
     return new GenericContainer<>("bmedora/hadoop:2.9-base")
-        .withEnv(
-            "HADOOP_NAMENODE_OPTS",
-            "-Dcom.sun.management.jmxremote.port="
-                + jmxPort
-                + " -Dcom.sun.management.jmxremote.rmi.port="
-                + jmxPort
-                + " -Dcom.sun.management.jmxremote.ssl=false"
-                + " -Dcom.sun.management.jmxremote.authenticate=false"
-                + " -Dcom.sun.management.jmxremote.local.only=false")
-        .withStartupTimeout(Duration.ofMinutes(20))
-//        .withExposedPorts(jmxPort, 10020, 19888, 50010, 50020, 50070, 50075, 50090, 8020, 8042, 8088, 9000)
-        .withExposedPorts(jmxPort, 8004)
+        .withCopyFileToContainer(
+            MountableFile.forClasspathResource("hadoop-env.sh", 0400),
+            "/hadoop/etc/hadoop/hadoop-env.sh")
+        .waitingFor(Wait.forListeningPort().withStartupTimeout(Duration.ofMinutes(200)))
+        .withExposedPorts(jmxPort)
         .withCreateContainerCmdModifier(cmd -> cmd.withHostName("test-host"))
         .waitingFor(Wait.forListeningPort());
   }
@@ -38,117 +35,15 @@ public class HadoopIntegrationTest extends TargetSystemIntegrationTest {
 
   @Override
   protected void verifyMetrics() {
-//    waitAndAssertMetrics(
-//        metric ->
-//            assertSumWithAttributes(
-//                metric,
-//                "hadoop.name_node.capacity.usage",
-//                "The current used capacity across all data nodes reporting to the name node.",
-//                "by",
-//                attrs -> attrs.contains(entry("node_name", "test-host")))
-//    );
-    //    waitAndAssertMetrics(
-    //        metric ->
-    //            assertGauge(
-    //                metric,
-    //                "cassandra.client.request.range_slice.latency.50p",
-    //                "Token range read request latency - 50th percentile",
-    //                "us"),
-    //        metric ->
-    //            assertGauge(
-    //                metric,
-    //                "cassandra.client.request.range_slice.latency.99p",
-    //                "Token range read request latency - 99th percentile",
-    //                "us"),
-    //        metric ->
-    //            assertGauge(
-    //                metric,
-    //                "cassandra.client.request.range_slice.latency.max",
-    //                "Maximum token range read request latency",
-    //                "us"),
-    //        metric ->
-    //            assertGauge(
-    //                metric,
-    //                "cassandra.client.request.read.latency.50p",
-    //                "Standard read request latency - 50th percentile",
-    //                "us"),
-    //        metric ->
-    //            assertGauge(
-    //                metric,
-    //                "cassandra.client.request.read.latency.99p",
-    //                "Standard read request latency - 99th percentile",
-    //                "us"),
-    //        metric ->
-    //            assertGauge(
-    //                metric,
-    //                "cassandra.client.request.read.latency.max",
-    //                "Maximum standard read request latency",
-    //                "us"),
-    //        metric ->
-    //            assertGauge(
-    //                metric,
-    //                "cassandra.client.request.write.latency.50p",
-    //                "Regular write request latency - 50th percentile",
-    //                "us"),
-    //        metric ->
-    //            assertGauge(
-    //                metric,
-    //                "cassandra.client.request.write.latency.99p",
-    //                "Regular write request latency - 99th percentile",
-    //                "us"),
-    //        metric ->
-    //            assertGauge(
-    //                metric,
-    //                "cassandra.client.request.write.latency.max",
-    //                "Maximum regular write request latency",
-    //                "us"),
-    //        metric ->
-    //            assertSum(
-    //                metric,
-    //                "cassandra.compaction.tasks.completed",
-    //                "Number of completed compactions since server [re]start",
-    //                "1"),
-    //        metric ->
-    //            assertGauge(
-    //                metric,
-    //                "cassandra.compaction.tasks.pending",
-    //                "Estimated number of compactions remaining to perform",
-    //                "1"),
-    //        metric ->
-    //            assertSum(
-    //                metric,
-    //                "cassandra.storage.load.count",
-    //                "Size of the on disk data size this node manages",
-    //                "by",
-    //                /* isMonotonic= */ false),
-    //        metric ->
-    //            assertSum(
-    //                metric,
-    //                "cassandra.storage.total_hints.count",
-    //                "Number of hint messages written to this node since [re]start",
-    //                "1"),
-    //        metric ->
-    //            assertSum(
-    //                metric,
-    //                "cassandra.storage.total_hints.in_progress.count",
-    //                "Number of hints attempting to be sent currently",
-    //                "1",
-    //                /* isMonotonic= */ false),
-    //        metric ->
-    //            assertSumWithAttributes(
-    //                metric,
-    //                "cassandra.client.request.count",
-    //                "Number of requests by operation",
-    //                "1",
-    //                attrs -> attrs.containsOnly(entry("operation", "RangeSlice")),
-    //                attrs -> attrs.containsOnly(entry("operation", "Read")),
-    //                attrs -> attrs.containsOnly(entry("operation", "Write"))),
-    //        metric ->
-    //            assertSumWithAttributes(
-    //                metric,
-    //                "cassandra.client.request.error.count",
-    //                "Number of request errors by operation",
-    //                "1",
-    //                getRequestErrorCountAttributes()));
+    waitAndAssertMetrics(
+        metric ->
+            assertSumWithAttributes(
+                metric,
+                "hadoop.name_node.capacity.usage",
+                "The current used capacity across all data nodes reporting to the name node.",
+                "by",
+                attrs -> attrs.contains(entry("node_name", "test-host")))
+    );
   }
 }
+
