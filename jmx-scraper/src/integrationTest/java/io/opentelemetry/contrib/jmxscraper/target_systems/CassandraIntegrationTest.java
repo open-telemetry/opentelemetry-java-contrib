8cb96ad9bacad215ca26bed0c0e372ad3136bd64
robsunday <rniedziela@splunk.com>
2024-11-25 09:22:01 +0100

    Cassandra integration test converted

--- jmx-scraper/src/integrationTest/java/io/opentelemetry/contrib/jmxscraper/target_systems/CassandraIntegrationTest.java
+++ jmx-scraper/src/integrationTest/java/io/opentelemetry/contrib/jmxscraper/target_systems/CassandraIntegrationTest.java
@@ -5,9 +5,6 @@
 
 package io.opentelemetry.contrib.jmxscraper.target_systems;
 
-import static io.opentelemetry.contrib.jmxscraper.target_systems.MetricAssertions.assertGauge;
-import static io.opentelemetry.contrib.jmxscraper.target_systems.MetricAssertions.assertSum;
-import static io.opentelemetry.contrib.jmxscraper.target_systems.MetricAssertions.assertSumWithAttributes;
 import static org.assertj.core.api.Assertions.entry;
 
 import io.opentelemetry.contrib.jmxscraper.JmxScraperContainer;
@@ -44,110 +41,74 @@ public class CassandraIntegrationTest extends TargetSystemIntegrationTest {
   }
 
   @Override
-  protected void verifyMetrics() {
-    waitAndAssertMetrics(
-        metric ->
-            assertGauge(
-                metric,
-                "cassandra.client.request.range_slice.latency.50p",
-                "Token range read request latency - 50th percentile",
-                "us"),
-        metric ->
-            assertGauge(
-                metric,
-                "cassandra.client.request.range_slice.latency.99p",
-                "Token range read request latency - 99th percentile",
-                "us"),
-        metric ->
-            assertGauge(
-                metric,
-                "cassandra.client.request.range_slice.latency.max",
-                "Maximum token range read request latency",
-                "us"),
-        metric ->
-            assertGauge(
-                metric,
-                "cassandra.client.request.read.latency.50p",
-                "Standard read request latency - 50th percentile",
-                "us"),
-        metric ->
-            assertGauge(
-                metric,
-                "cassandra.client.request.read.latency.99p",
-                "Standard read request latency - 99th percentile",
-                "us"),
-        metric ->
-            assertGauge(
-                metric,
-                "cassandra.client.request.read.latency.max",
-                "Maximum standard read request latency",
-                "us"),
-        metric ->
-            assertGauge(
-                metric,
-                "cassandra.client.request.write.latency.50p",
-                "Regular write request latency - 50th percentile",
-                "us"),
-        metric ->
-            assertGauge(
-                metric,
-                "cassandra.client.request.write.latency.99p",
-                "Regular write request latency - 99th percentile",
-                "us"),
-        metric ->
-            assertGauge(
-                metric,
-                "cassandra.client.request.write.latency.max",
-                "Maximum regular write request latency",
-                "us"),
-        metric ->
-            assertSum(
-                metric,
-                "cassandra.compaction.tasks.completed",
-                "Number of completed compactions since server [re]start",
-                "1"),
-        metric ->
-            assertGauge(
-                metric,
-                "cassandra.compaction.tasks.pending",
-                "Estimated number of compactions remaining to perform",
-                "1"),
-        metric ->
-            assertSum(
-                metric,
-                "cassandra.storage.load.count",
-                "Size of the on disk data size this node manages",
-                "by",
-                /* isMonotonic= */ false),
-        metric ->
-            assertSum(
-                metric,
-                "cassandra.storage.total_hints.count",
-                "Number of hint messages written to this node since [re]start",
-                "1"),
-        metric ->
-            assertSum(
-                metric,
-                "cassandra.storage.total_hints.in_progress.count",
-                "Number of hints attempting to be sent currently",
-                "1",
-                /* isMonotonic= */ false),
-        metric ->
-            assertSumWithAttributes(
-                metric,
-                "cassandra.client.request.count",
-                "Number of requests by operation",
-                "1",
-                attrs -> attrs.containsOnly(entry("operation", "RangeSlice")),
-                attrs -> attrs.containsOnly(entry("operation", "Read")),
-                attrs -> attrs.containsOnly(entry("operation", "Write"))),
-        metric ->
-            assertSumWithAttributes(
-                metric,
-                "cassandra.client.request.error.count",
-                "Number of request errors by operation",
-                "1",
-                getRequestErrorCountAttributes()));
+  protected MetricsVerifier createMetricsVerifier() {
+    return MetricsVerifier.create()
+        .assertGauge(
+            "cassandra.client.request.range_slice.latency.50p",
+            "Token range read request latency - 50th percentile",
+            "us")
+        .assertGauge(
+            "cassandra.client.request.range_slice.latency.99p",
+            "Token range read request latency - 99th percentile",
+            "us")
+        .assertGauge(
+            "cassandra.client.request.range_slice.latency.max",
+            "Maximum token range read request latency",
+            "us")
+        .assertGauge(
+            "cassandra.client.request.read.latency.50p",
+            "Standard read request latency - 50th percentile",
+            "us")
+        .assertGauge(
+            "cassandra.client.request.read.latency.99p",
+            "Standard read request latency - 99th percentile",
+            "us")
+        .assertGauge(
+            "cassandra.client.request.read.latency.max",
+            "Maximum standard read request latency",
+            "us")
+        .assertGauge(
+            "cassandra.client.request.write.latency.50p",
+            "Regular write request latency - 50th percentile",
+            "us")
+        .assertGauge(
+            "cassandra.client.request.write.latency.99p",
+            "Regular write request latency - 99th percentile",
+            "us")
+        .assertGauge(
+            "cassandra.client.request.write.latency.max",
+            "Maximum regular write request latency",
+            "us")
+        .assertCounter(
+            "cassandra.compaction.tasks.completed",
+            "Number of completed compactions since server [re]start",
+            "1")
+        .assertGauge(
+            "cassandra.compaction.tasks.pending",
+            "Estimated number of compactions remaining to perform",
+            "1")
+        .assertUpDownCounter(
+            "cassandra.storage.load.count", "Size of the on disk data size this node manages", "by")
+        .assertCounter(
+            "cassandra.storage.total_hints.count",
+            "Number of hint messages written to this node since [re]start",
+            "1")
+        .assertUpDownCounter(
+            "cassandra.storage.total_hints.in_progress.count",
+            "Number of hints attempting to be sent currently",
+            "1")
+        .assertCounterWithAttributes(
+            "cassandra.client.request.count",
+            "Number of requests by operation",
+            "1",
+            attrs -> attrs.containsOnly(entry("operation", "RangeSlice")),
+            attrs -> attrs.containsOnly(entry("operation", "Read")),
+            attrs -> attrs.containsOnly(entry("operation", "Write")))
+        .assertCounterWithAttributes(
+            "cassandra.client.request.error.count",
+            "Number of request errors by operation",
+            "1",
+            getRequestErrorCountAttributes());
   }
 
   @SuppressWarnings("unchecked")
