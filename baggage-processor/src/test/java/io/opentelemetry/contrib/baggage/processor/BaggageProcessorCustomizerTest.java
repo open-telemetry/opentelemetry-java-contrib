1e66b8cf7c8a13cbcacc81ac2327faecdc9b80d8
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-12-16 11:14:43 +0100

    increase timeout for tests - to tolerate high loads

--- baggage-processor/src/test/java/io/opentelemetry/contrib/baggage/processor/BaggageProcessorCustomizerTest.java
+++ baggage-processor/src/test/java/io/opentelemetry/contrib/baggage/processor/BaggageProcessorCustomizerTest.java
@@ -78,7 +78,7 @@ class BaggageProcessorCustomizerTest {
     }
 
     await()
-        .atMost(Duration.ofSeconds(1))
+        .atMost(Duration.ofSeconds(60))
         .untilAsserted(
             () -> {
               assertThat(spanExporter.getFinishedSpanItems())
