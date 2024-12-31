8745020111b0078d1ba164d26c9f00808eb28045
Pranav Sharma <sharmapranav@google.com>
2024-12-31 06:58:34 +0000

    Add integration smoke test

--- gcp-auth-extension/build.gradle.kts
+++ gcp-auth-extension/build.gradle.kts
@@ -2,10 +2,16 @@ plugins {
   id("otel.java-conventions")
   id("otel.publish-conventions")
   id("com.github.johnrengelman.shadow")
+  id("org.springframework.boot") version "2.7.18"
 }
 
 description = "OpenTelemetry Java Agent Extension that enables authentication support for OTLP exporters"
 otelJava.moduleName.set("io.opentelemetry.contrib.gcp.auth")
+
+val agent: Configuration by configurations.creating {
+  isCanBeResolved = true
+  isCanBeConsumed = false
+}
 
 dependencies {
   annotationProcessor("com.google.auto.service:auto-service")
@@ -29,12 +35,81 @@ dependencies {
   testImplementation("io.opentelemetry:opentelemetry-exporter-otlp")
   testImplementation("io.opentelemetry:opentelemetry-sdk-testing")
   testImplementation("io.opentelemetry:opentelemetry-sdk-extension-autoconfigure")
+  testImplementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations")
 
   testImplementation("org.awaitility:awaitility")
   testImplementation("org.mockito:mockito-inline")
   testImplementation("org.mockito:mockito-junit-jupiter")
+  testImplementation("org.mock-server:mockserver-netty:5.15.0")
+  testImplementation("io.opentelemetry.proto:opentelemetry-proto:1.4.0-alpha")
+  testImplementation("org.springframework.boot:spring-boot-starter-web:2.7.18")
+  testImplementation("org.springframework.boot:spring-boot-starter:2.7.18")
+  testImplementation("org.springframework.boot:spring-boot-starter-test:2.7.18")
+
+  agent("io.opentelemetry.javaagent:opentelemetry-javaagent")
 }
 
-tasks.test {
+tasks {
+  test {
+    useJUnitPlatform()
+    // exclude integration test
+    exclude("io/opentelemetry/contrib/gcp/auth/GcpAuthExtensionEndToEndTest.class")
+  }
+
+  shadowJar {
+    archiveFileName.set("gcp-auth-extension.jar")
+  }
+
+  jar {
+    // Disable standard jar
+    enabled = false
+  }
+
+  assemble {
+    dependsOn(shadowJar)
+  }
+
+  bootJar {
+    // disable bootJar in build since it only runs as part of test
+    enabled = false
+  }
+}
+
+val builtLibsDir = layout.buildDirectory.dir("libs").get().asFile.absolutePath
+val javaAgentJarPath = "$builtLibsDir/otel-agent.jar"
+val authExtensionJarPath = "$builtLibsDir/gcp-auth-extension.jar"
+
+tasks.register<Copy>("copyAgent") {
+  into(layout.buildDirectory.dir("libs"))
+  from(configurations.named("agent") {
+    rename("opentelemetry-javaagent(.*).jar", "otel-agent.jar")
+  })
+}
+
+tasks.register<Test>("IntegrationTest") {
+  dependsOn(tasks.shadowJar)
+  dependsOn(tasks.named("copyAgent"))
+
   useJUnitPlatform()
+  // include only the integration test file
+  include("io/opentelemetry/contrib/gcp/auth/GcpAuthExtensionEndToEndTest.class")
+
+  val fakeCredsFilePath = project.file("src/test/resources/fakecreds.json").absolutePath
+
+  environment("GOOGLE_CLOUD_QUOTA_PROJECT", "quota-project-id")
+  environment("GOOGLE_APPLICATION_CREDENTIALS", fakeCredsFilePath)
+  jvmArgs = listOf(
+    "-javaagent:$javaAgentJarPath",
+    "-Dotel.javaagent.extensions=$authExtensionJarPath",
+    "-Dgoogle.cloud.project=my-gcp-project",
+    "-Dotel.java.global-autoconfigure.enabled=true",
+    "-Dotel.exporter.otlp.endpoint=http://localhost:4318",
+    "-Dotel.resource.providers.gcp.enabled=true",
+    "-Dotel.traces.exporter=otlp",
+    "-Dotel.bsp.schedule.delay=2000",
+    "-Dotel.metrics.exporter=none",
+    "-Dotel.logs.exporter=none",
+    "-Dotel.exporter.otlp.protocol=http/protobuf",
+    "-Dmockserver.logLevel=off"
+  )
 }
