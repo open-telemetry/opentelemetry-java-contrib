96c625020d2a2cbd64fb61ee7aef8b93bd177d29
Sylvain Juge <763082+SylvainJuge@users.noreply.github.com>
2025-01-08 22:00:12 +0100

    add some test coverage

--- jmx-scraper/src/test/java/io/opentelemetry/contrib/jmxscraper/config/PropertiesCustomizerTest.java
+++ jmx-scraper/src/test/java/io/opentelemetry/contrib/jmxscraper/config/PropertiesCustomizerTest.java
@@ -6,6 +6,7 @@
 package io.opentelemetry.contrib.jmxscraper.config;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import io.opentelemetry.sdk.autoconfigure.spi.ConfigProperties;
 import io.opentelemetry.sdk.autoconfigure.spi.internal.DefaultConfigProperties;
@@ -14,6 +15,12 @@ import java.util.Map;
 import org.junit.jupiter.api.Test;
 
 class PropertiesCustomizerTest {
+
+  @Test
+  void tryGetConfigBeforeApply() {
+    assertThatThrownBy(()-> new PropertiesCustomizer().getScraperConfig())
+        .isInstanceOf(IllegalStateException.class);
+  }
 
   @Test
   void defaultLoggingExporter() {
@@ -36,5 +43,19 @@ class PropertiesCustomizerTest {
 
     PropertiesCustomizer customizer = new PropertiesCustomizer();
     assertThat(customizer.apply(config)).isEmpty();
+  }
+
+  @Test
+  void getDefaultConfig() {
+    Map<String, String> map = new HashMap<>();
+    map.put("otel.jmx.service.url", "dummy-url");
+    map.put("otel.jmx.target.system", "jvm");
+    map.put("otel.metrics.exporter", "otlp");
+    ConfigProperties config = DefaultConfigProperties.createFromMap(map);
+
+    PropertiesCustomizer customizer = new PropertiesCustomizer();
+    assertThat(customizer.apply(config)).isEmpty();
+
+    assertThat(customizer.getScraperConfig()).isNotNull();
   }
 }
