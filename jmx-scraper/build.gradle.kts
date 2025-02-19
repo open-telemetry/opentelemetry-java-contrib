4ce7187391ae59d99d698999b73c59c2e39d6f35
SylvainJuge <763082+SylvainJuge@users.noreply.github.com>
2025-02-19 22:51:42 +0100

    jmx-scraper add ssl support (#1710)

--- jmx-scraper/build.gradle.kts
+++ jmx-scraper/build.gradle.kts
@@ -37,6 +37,8 @@ testing {
         implementation("com.linecorp.armeria:armeria-junit5")
         implementation("com.linecorp.armeria:armeria-grpc")
         implementation("io.opentelemetry.proto:opentelemetry-proto:1.5.0-alpha")
+        implementation("org.bouncycastle:bcprov-jdk18on:1.80")
+        implementation("org.bouncycastle:bcpkix-jdk18on:1.80")
       }
     }
   }
