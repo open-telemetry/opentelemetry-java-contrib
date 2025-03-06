8f9c92a6bedad78fe656693697c3cf2404d93d0e
Sylvain Juge <763082+SylvainJuge@users.noreply.github.com>
2025-03-06 16:02:12 +0100

    minor cleanup

--- jmx-scraper/src/main/java/io/opentelemetry/contrib/jmxscraper/config/JmxScraperConfig.java
+++ jmx-scraper/src/main/java/io/opentelemetry/contrib/jmxscraper/config/JmxScraperConfig.java
@@ -43,7 +43,6 @@ public class JmxScraperConfig {
   static final String JMX_USERNAME = "otel.jmx.username";
   static final String JMX_PASSWORD = "otel.jmx.password";
 
-  // TODO: document those when they will be supported
   static final String JMX_REGISTRY_SSL = "otel.jmx.remote.registry.ssl";
   static final String JMX_REMOTE_PROFILE = "otel.jmx.remote.profile";
   static final String JMX_REALM = "otel.jmx.realm";
@@ -72,7 +71,7 @@ public class JmxScraperConfig {
     INSTRUMENTATION,
     LEGACY;
 
-    public static TargetSystemSource fromString(String source) {
+    static TargetSystemSource fromString(String source) {
       try {
         return TargetSystemSource.valueOf(source.toUpperCase(Locale.ROOT));
       } catch (IllegalArgumentException e) {
