578e121d1f4228e2bfa6f019a4c9f4c73f3c6930
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2025-04-08 13:35:09 +0200

    Downloading opamp protos

--- opamp-client/build.gradle.kts
+++ opamp-client/build.gradle.kts
@@ -1,5 +1,10 @@
+import de.undercouch.gradle.tasks.download.Download
+import de.undercouch.gradle.tasks.download.DownloadExtension
+import groovy.json.JsonSlurper
+
 plugins {
   id("otel.java-conventions")
+  id("de.undercouch.download") version "5.6.0"
   id("com.google.protobuf") version "0.9.4"
 }
 
@@ -39,4 +44,54 @@ dependencies {
   implementation("com.google.protobuf:protobuf-java:$protobufVersion")
   annotationProcessor("com.google.auto.value:auto-value")
   compileOnly("com.google.auto.value:auto-value-annotations")
+}
+
+val opampReleaseInfo = tasks.register<Download>("opampLastReleaseInfo") {
+  group = "opamp"
+  src("https://api.github.com/repos/open-telemetry/opamp-spec/releases/latest")
+  dest(project.layout.buildDirectory.file("$name/release.json"))
+}
+
+tasks.register<DownloadOpampProtos>("opampProtoDownload", download).configure {
+  group = "opamp"
+  dependsOn(opampReleaseInfo)
+  lastReleaseInfoJson.set {
+    opampReleaseInfo.get().dest
+  }
+  outputProtosDir.set(project.layout.buildDirectory.dir(name))
+  downloadedZipFile.set(project.layout.buildDirectory.file("intermediate/$name/release.zip"))
+}
+
+abstract class DownloadOpampProtos @Inject constructor(
+  private val download: DownloadExtension,
+  private val archiveOps: ArchiveOperations,
+  private val fileOps: FileSystemOperations,
+) : DefaultTask() {
+
+  @get:InputFile
+  abstract val lastReleaseInfoJson: RegularFileProperty
+
+  @get:OutputDirectory
+  abstract val outputProtosDir: DirectoryProperty
+
+  @get:Internal
+  abstract val downloadedZipFile: RegularFileProperty
+
+  @Suppress("UNCHECKED_CAST")
+  @TaskAction
+  fun execute() {
+    val releaseInfo = JsonSlurper().parse(lastReleaseInfoJson.get().asFile) as Map<String, String>
+    val zipUrl = releaseInfo["zipball_url"]
+    download.run {
+      src(zipUrl)
+      dest(downloadedZipFile)
+    }
+    val protos = archiveOps.zipTree(downloadedZipFile).matching {
+      setIncludes(listOf("**/*.proto"))
+    }
+    fileOps.sync {
+      from(protos)
+      into(outputProtosDir)
+    }
+  }
 }
