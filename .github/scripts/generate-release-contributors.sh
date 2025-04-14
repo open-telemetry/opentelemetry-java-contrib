9abec242c47a35018c79323af007e64d13bba5b0
Trask Stalnaker <trask.stalnaker@gmail.com>
2025-04-14 13:03:33 -0700

    Remove unneeded opentelemetrybot from script

--- .github/scripts/generate-release-contributors.sh
+++ .github/scripts/generate-release-contributors.sh
@@ -87,6 +87,5 @@ echo $contributors1 $contributors2 \
   | grep -v github-actions \
   | grep -v dependabot \
   | grep -v renovate \
-  | grep -v opentelemetrybot \
   | grep -v otelbot \
   | sed 's/^/@/'
