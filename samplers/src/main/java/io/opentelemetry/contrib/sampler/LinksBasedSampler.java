3f246368fa22e659b777fffb4bfdadc38a1f911c
otelbot <197425009+otelbot@users.noreply.github.com>
2025-05-19 22:29:17 +0000

    ./gradlew spotlessApply

--- samplers/src/main/java/io/opentelemetry/contrib/sampler/LinksBasedSampler.java
+++ samplers/src/main/java/io/opentelemetry/contrib/sampler/LinksBasedSampler.java
@@ -11,7 +11,6 @@ import io.opentelemetry.context.Context;
 import io.opentelemetry.sdk.trace.data.LinkData;
 import io.opentelemetry.sdk.trace.samplers.Sampler;
 import io.opentelemetry.sdk.trace.samplers.SamplingResult;
-
 import java.util.List;
 
 /**
@@ -20,44 +19,43 @@ import java.util.List;
  * links, this Sampler will use the "root" sampler that it is built with.
  */
 public final class LinksBasedSampler implements Sampler {
-    private final Sampler root;
+  private final Sampler root;
 
-    private LinksBasedSampler(Sampler root) {
-        this.root = root;
-    }
+  private LinksBasedSampler(Sampler root) {
+    this.root = root;
+  }
 
-    public static LinksBasedSampler create(
-            Sampler root) {
-        return new LinksBasedSampler(root);
-    }
+  public static LinksBasedSampler create(Sampler root) {
+    return new LinksBasedSampler(root);
+  }
 
-    @Override
-    public SamplingResult shouldSample(
-            Context parentContext,
-            String traceId,
-            String name,
-            SpanKind spanKind,
-            Attributes attributes,
-            List<LinkData> parentLinks) {
-        if (parentLinks.size() > 0) {
-            for (LinkData linkData : parentLinks) {
-                if (linkData.getSpanContext().isSampled()) {
-                    return SamplingResult.recordAndSample();
-                }
-            }
-            return SamplingResult.drop();
+  @Override
+  public SamplingResult shouldSample(
+      Context parentContext,
+      String traceId,
+      String name,
+      SpanKind spanKind,
+      Attributes attributes,
+      List<LinkData> parentLinks) {
+    if (parentLinks.size() > 0) {
+      for (LinkData linkData : parentLinks) {
+        if (linkData.getSpanContext().isSampled()) {
+          return SamplingResult.recordAndSample();
         }
-
-        return this.root.shouldSample(parentContext, traceId, name, spanKind, attributes, parentLinks);
+      }
+      return SamplingResult.drop();
     }
 
-    @Override
-    public String getDescription() {
-        return String.format("LinksBased{root:%s}", this.root.getDescription());
-    }
+    return this.root.shouldSample(parentContext, traceId, name, spanKind, attributes, parentLinks);
+  }
 
-    @Override
-    public String toString() {
-        return getDescription();
-    }
+  @Override
+  public String getDescription() {
+    return String.format("LinksBased{root:%s}", this.root.getDescription());
+  }
+
+  @Override
+  public String toString() {
+    return getDescription();
+  }
 }
