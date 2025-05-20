dda8cbf9d9647c2103d20f1ef3035ae3110c26b2
César <56847527+LikeTheSalad@users.noreply.github.com>
2025-05-20 16:16:50 +0200

    Opamp client api (#1835)

--- opamp-client/build.gradle.kts
+++ opamp-client/build.gradle.kts
@@ -1,11 +1,75 @@
+import de.undercouch.gradle.tasks.download.Download
+import de.undercouch.gradle.tasks.download.DownloadExtension
+import groovy.json.JsonSlurper
+
 plugins {
   id("otel.java-conventions")
+  id("de.undercouch.download") version "5.6.0"
+  id("com.squareup.wire") version "5.3.1"
 }
 
 description = "Client implementation of the OpAMP spec."
 otelJava.moduleName.set("io.opentelemetry.contrib.opamp.client")
 
-java {
-  sourceCompatibility = JavaVersion.VERSION_1_8
-  targetCompatibility = JavaVersion.VERSION_1_8
+dependencies {
+  annotationProcessor("com.google.auto.value:auto-value")
+  compileOnly("com.google.auto.value:auto-value-annotations")
+}
+
+val opampReleaseInfo = tasks.register<Download>("opampLastReleaseInfo") {
+  group = "opamp"
+  src("https://api.github.com/repos/open-telemetry/opamp-spec/releases/latest")
+  dest(project.layout.buildDirectory.file("opamp/release.json"))
+}
+
+val opampProtos = tasks.register<DownloadOpampProtos>("opampProtoDownload", download)
+opampProtos.configure {
+  group = "opamp"
+  dependsOn(opampReleaseInfo)
+  lastReleaseInfoJson.set {
+    opampReleaseInfo.get().dest
+  }
+  outputProtosDir.set(project.layout.buildDirectory.dir("opamp/protos"))
+  downloadedZipFile.set(project.layout.buildDirectory.file("intermediate/$name/release.zip"))
+}
+
+wire {
+  java {}
+  sourcePath {
+    srcDir(opampProtos)
+  }
+}
+
+abstract class DownloadOpampProtos @Inject constructor(
+  private val download: DownloadExtension,
+  private val archiveOps: ArchiveOperations,
+  private val fileOps: FileSystemOperations,
+) : DefaultTask() {
+
+  @get:InputFile
+  abstract val lastReleaseInfoJson: RegularFileProperty
+
+  @get:OutputDirectory
+  abstract val outputProtosDir: DirectoryProperty
+
+  @get:Internal
+  abstract val downloadedZipFile: RegularFileProperty
+
+  @Suppress("UNCHECKED_CAST")
+  @TaskAction
+  fun execute() {
+    val releaseInfo = JsonSlurper().parse(lastReleaseInfoJson.get().asFile) as Map<String, String>
+    val zipUrl = releaseInfo["zipball_url"]
+    download.run {
+      src(zipUrl)
+      dest(downloadedZipFile)
+    }
+    val protos = archiveOps.zipTree(downloadedZipFile).matching {
+      setIncludes(listOf("**/*.proto"))
+    }
+    fileOps.sync {
+      from(protos.files)
+      into(outputProtosDir)
+    }
+  }
 }
