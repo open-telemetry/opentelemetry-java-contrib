90e0f5fad0840829e70c294c1925d8a2f1fd83e8
Jason Plumb <jplumb@splunk.com>
2025-06-13 13:31:05 -0700

    cleanup and spotless

--- ibm-mq-metrics/src/main/java/io/opentelemetry/ibm/mq/opentelemetry/ConfigWrapper.java
+++ ibm-mq-metrics/src/main/java/io/opentelemetry/ibm/mq/opentelemetry/ConfigWrapper.java
@@ -1,24 +1,16 @@
 /*
- * Copyright Splunk Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
  */
+
 package io.opentelemetry.ibm.mq.opentelemetry;
 
 import static java.util.Collections.emptyList;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.Duration;
 import java.util.Collections;
 import java.util.List;
@@ -44,9 +36,10 @@ public final class ConfigWrapper {
     this.config = config;
   }
 
-  public static ConfigWrapper parse(String configFile) throws FileNotFoundException {
+  public static ConfigWrapper parse(String configFile) throws IOException {
     Yaml yaml = new Yaml();
-    Map<String, ?> config = yaml.load(new FileReader(configFile));
+    Map<String, ?> config =
+        yaml.load(Files.newBufferedReader(Paths.get(configFile), Charset.defaultCharset()));
     return new ConfigWrapper(config);
   }
 
@@ -121,7 +114,7 @@ public final class ConfigWrapper {
     return (Map<String, ?>) metrics;
   }
 
-  private int defaultedInt(Map<String, ?> section, String key, int defaultValue) {
+  private static int defaultedInt(Map<String, ?> section, String key, int defaultValue) {
     Object val = section.get(key);
     return val instanceof Integer ? (Integer) val : defaultValue;
   }
