73cdcf5f66ab2cbb28d01f30d4c2c4a505cb0ae5
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-07-09 16:49:10 +0200

    maven component provider

---
+++ maven-extension/src/test/java/io/opentelemetry/maven/resources/ResourceComponentProviderTest.java
@@ -0,0 +1,20 @@
+package io.opentelemetry.maven.resources;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.opentelemetry.sdk.autoconfigure.internal.SpiHelper;
+import io.opentelemetry.sdk.autoconfigure.spi.internal.ComponentProvider;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class ResourceComponentProviderTest {
+
+  @Test
+  @SuppressWarnings("rawtypes")
+  void providerIsLoaded() {
+    List<ComponentProvider> providers = SpiHelper.create(
+            ResourceComponentProviderTest.class.getClassLoader())
+        .load(ComponentProvider.class);
+    assertThat(providers).extracting(ComponentProvider::getName).contains("maven");
+  }
+}
