c04386b52051ae9844b75c62f12129be89deb7ac
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-07-15 14:00:40 +0200

    make temp dir more reliable

--- inferred-spans/src/test/java/io/opentelemetry/contrib/inferredspans/internal/SamplingProfilerTest.java
+++ inferred-spans/src/test/java/io/opentelemetry/contrib/inferredspans/internal/SamplingProfilerTest.java
@@ -32,12 +32,12 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.DisabledForJreRange;
 import org.junit.jupiter.api.condition.DisabledOnOs;
 import org.junit.jupiter.api.condition.JRE;
 import org.junit.jupiter.api.condition.OS;
+import org.junit.jupiter.api.io.TempDir;
 
 // async-profiler doesn't work on Windows
 @DisabledOnOs(OS.WINDOWS)
@@ -46,11 +46,7 @@ class SamplingProfilerTest {
 
   private ProfilerTestSetup setup;
 
-  @BeforeEach
-  void setup() {
-    // avoids any test failure to make other tests to fail
-    getProfilerTempFiles().forEach(SamplingProfilerTest::silentDeleteFile);
-  }
+  @TempDir private Path tempDir;
 
   @AfterEach
   void tearDown() {
@@ -58,7 +54,6 @@ class SamplingProfilerTest {
       setup.close();
       setup = null;
     }
-    getProfilerTempFiles().forEach(SamplingProfilerTest::silentDeleteFile);
   }
 
   @Test
@@ -117,8 +112,8 @@ class SamplingProfilerTest {
       defaultConfig = ProfilerTestSetup.extractProfilerImpl(profiler1).getConfig();
     }
 
-    Path tempFile1 = Files.createTempFile("otel-inferred-provided", "test.bin");
-    Path tempFile2 = Files.createTempFile("otel-inferred-provided", "test.jfr");
+    Path tempFile1 = Files.createTempFile(tempDir, "otel-inferred-provided", "test.bin");
+    Path tempFile2 = Files.createTempFile(tempDir, "otel-inferred-provided", "test.jfr");
 
     try (OpenTelemetrySdk sdk = OpenTelemetrySdk.builder().build()) {
 
@@ -128,7 +123,8 @@ class SamplingProfilerTest {
               new FixedClock(),
               () -> sdk.getTracer("my-tracer"),
               tempFile1.toFile(),
-              tempFile2.toFile());
+              tempFile2.toFile(),
+              tempDir.toFile());
 
       otherProfiler.start();
       awaitProfilerStarted(otherProfiler);
@@ -332,21 +328,5 @@ class SamplingProfilerTest {
                   .samplingInterval(Duration.ofMillis(5));
               configCustomizer.accept(config);
             });
-  }
-
-  private static void awaitProfilerStarted(SamplingProfiler profiler) {
-    // ensure profiler is initialized
-    await()
-        .pollDelay(Duration.ofMillis(10))
-        .timeout(Duration.ofSeconds(6))
-        .until(() -> profiler.getProfilingSessions() > 1);
-  }
-
-  private static void silentDeleteFile(Path f) {
-    try {
-      Files.delete(f);
-    } catch (IOException e) {
-      throw new IllegalStateException(e);
-    }
   }
 }
