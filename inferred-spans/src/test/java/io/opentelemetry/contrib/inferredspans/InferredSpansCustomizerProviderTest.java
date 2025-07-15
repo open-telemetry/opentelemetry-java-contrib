91168149357bea4386b9f2c51aa4128473daab23
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-07-15 10:02:24 +0200

    inferred spans

--- inferred-spans/src/test/java/io/opentelemetry/contrib/inferredspans/InferredSpansCustomizerProviderTest.java
+++ inferred-spans/src/test/java/io/opentelemetry/contrib/inferredspans/InferredSpansCustomizerProviderTest.java
@@ -5,37 +5,40 @@
 
 package io.opentelemetry.contrib.inferredspans;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import io.opentelemetry.sdk.extension.incubator.fileconfig.DeclarativeConfiguration;
-import io.opentelemetry.sdk.extension.incubator.fileconfig.internal.model.OpenTelemetryConfigurationModel;
+import org.junit.jupiter.api.Test;
+
 import java.io.ByteArrayInputStream;
 import java.nio.charset.StandardCharsets;
-import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class InferredSpansCustomizerProviderTest {
 
+  private static final String FQDN =
+      "spanProcessor=io.opentelemetry.contrib.inferredspans.InferredSpansProcessor";
+
   @Test
-  void declarativeConfig() {
+  void enabled() {
+    assertThat(create("")).contains(FQDN);
+  }
+
+  @Test
+  void disabled() {
+    assertThat(create("enabled: false")).doesNotContain(FQDN);
+  }
+
+  private static String create(String enabled) {
     String yaml =
         "file_format: 0.4\n"
             + "tracer_provider:\n"
-            + "instrumentation/development:\n"
-            + "  java:\n"
-            + "    inferred_spans:\n"
-            + "      enabled: true\n";
-
-    OpenTelemetryConfigurationModel model =
-        DeclarativeConfiguration.parse(
-            new ByteArrayInputStream(yaml.getBytes(StandardCharsets.UTF_8)));
-
-    new InferredSpansCustomizerProvider()
-        .customize(
-            c -> {
-              OpenTelemetryConfigurationModel configurationModel = c.apply(model);
-              assertThat(configurationModel.toString())
-                  .matches(
-                      ".*SpanProcessorModel@.{8}\\[batch=<null>,simple=<null>,additionalProperties=\\{inferred_spans=null}.*");
-            });
+            + "  processors:\n"
+            + "    - inferred_spans:\n"
+            + "        "
+            + enabled
+            + "\n";
+    return DeclarativeConfiguration.parseAndCreate(
+            new ByteArrayInputStream(yaml.getBytes(StandardCharsets.UTF_8)))
+        .toString();
   }
 }
