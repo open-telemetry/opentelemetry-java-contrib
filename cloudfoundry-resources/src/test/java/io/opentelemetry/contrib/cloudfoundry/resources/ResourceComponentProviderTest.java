2084d65ebfb5a0f0eacf810f3b23857c5aeb4cbc
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-07-21 14:02:10 +0200

    use component loader

--- cloudfoundry-resources/src/test/java/io/opentelemetry/contrib/cloudfoundry/resources/ResourceComponentProviderTest.java
+++ cloudfoundry-resources/src/test/java/io/opentelemetry/contrib/cloudfoundry/resources/ResourceComponentProviderTest.java
@@ -7,9 +7,8 @@ package io.opentelemetry.contrib.cloudfoundry.resources;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import io.opentelemetry.sdk.autoconfigure.internal.SpiHelper;
+import io.opentelemetry.common.ComponentLoader;
 import io.opentelemetry.sdk.autoconfigure.spi.internal.ComponentProvider;
-import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class ResourceComponentProviderTest {
@@ -17,8 +16,8 @@ public class ResourceComponentProviderTest {
   @Test
   @SuppressWarnings("rawtypes")
   void providerIsLoaded() {
-    List<ComponentProvider> providers =
-        SpiHelper.create(ResourceComponentProviderTest.class.getClassLoader())
+    Iterable<ComponentProvider> providers =
+        ComponentLoader.forClassLoader(ResourceComponentProviderTest.class.getClassLoader())
             .load(ComponentProvider.class);
     assertThat(providers).extracting(ComponentProvider::getName).containsExactly("cloud_foundry");
   }
