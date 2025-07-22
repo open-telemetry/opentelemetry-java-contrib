7bf95dfd1b0f25a9770e5233d7b22ac5bd13d87d
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-07-22 18:36:13 +0200

    add experimental- suffix, add test

---
+++ span-stacktrace/src/test/java/io/opentelemetry/contrib/stacktrace/StackTraceComponentProviderTest.java
@@ -0,0 +1,27 @@
+package io.opentelemetry.contrib.stacktrace;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.extension.incubator.fileconfig.DeclarativeConfiguration;
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
+import org.junit.jupiter.api.Test;
+
+class StackTraceComponentProviderTest {
+  @Test
+   void endToEnd() {
+     String yaml =
+         "file_format: 0.4\n"
+             + "tracer_provider:\n"
+             + "  processors:\n"
+             + "    - experimental-stacktrace:\n";
+
+     OpenTelemetrySdk openTelemetrySdk =
+         DeclarativeConfiguration.parseAndCreate(
+             new ByteArrayInputStream(yaml.getBytes(StandardCharsets.UTF_8)));
+
+    assertThat(openTelemetrySdk.getSdkTracerProvider().toString())
+        .contains("StackTraceSpanProcessor");
+   }
+}
