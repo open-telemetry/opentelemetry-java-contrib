9f48aad49e8ec12fb7c067bb356cc0f2a66d2bf6
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-07-22 11:24:36 +0200

    add end to end test for declarative config for aws

--- aws-resources/build.gradle.kts
+++ aws-resources/build.gradle.kts
@@ -21,11 +21,25 @@ dependencies {
   implementation("com.squareup.okhttp3:okhttp")
 
   testImplementation("io.opentelemetry:opentelemetry-sdk-extension-autoconfigure")
+  testImplementation("io.opentelemetry:opentelemetry-sdk-extension-incubator")
   testImplementation("io.opentelemetry:opentelemetry-api-incubator")
   testImplementation("io.opentelemetry:opentelemetry-sdk-testing")
+  testImplementation("io.opentelemetry:opentelemetry-exporter-logging")
 
   testImplementation("com.linecorp.armeria:armeria-junit5")
   testRuntimeOnly("org.bouncycastle:bcpkix-jdk15on")
   testImplementation("com.google.guava:guava")
   testImplementation("org.skyscreamer:jsonassert")
+  testImplementation("org.junit-pioneer:junit-pioneer")
+}
+
+tasks {
+  withType<Test>().configureEach {
+    environment(
+      "AWS_REGION" to "us-east-1",
+      "AWS_LAMBDA_FUNCTION_NAME" to "my-function",
+      "AWS_LAMBDA_FUNCTION_VERSION" to "1.2.3"
+    )
+    jvmArgs("-Dotel.experimental.config.file=${project.projectDir.resolve("src/test/resources/config.yaml")}")
+  }
 }
