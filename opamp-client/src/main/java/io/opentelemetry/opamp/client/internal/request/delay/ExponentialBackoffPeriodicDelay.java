cc57339358dcb1509708c0042ec1e870c62eca0c
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2025-07-30 10:50:26 +0200

    Creating ExponentialBackoffPeriodicDelay

---
+++ opamp-client/src/main/java/io/opentelemetry/opamp/client/internal/request/delay/ExponentialBackoffPeriodicDelay.java
@@ -0,0 +1,33 @@
+package io.opentelemetry.opamp.client.internal.request.delay;
+
+import java.time.Duration;
+import javax.annotation.concurrent.GuardedBy;
+
+public final class ExponentialBackoffPeriodicDelay implements PeriodicDelay {
+  private final Duration initialDelay;
+  private final Object delayNanosLock = new Object();
+
+  @GuardedBy("delayNanosLock")
+  private long delayNanos;
+
+  public ExponentialBackoffPeriodicDelay(Duration initialDelay) {
+    this.initialDelay = initialDelay;
+    delayNanos = initialDelay.toNanos();
+  }
+
+  @Override
+  public Duration getNextDelay() {
+    synchronized (delayNanosLock) {
+      long previousValue = delayNanos;
+      delayNanos = delayNanos * 2;
+      return Duration.ofNanos(previousValue);
+    }
+  }
+
+  @Override
+  public void reset() {
+    synchronized (delayNanosLock) {
+      delayNanos = initialDelay.toNanos();
+    }
+  }
+}
---
+++ opamp-client/src/test/java/io/opentelemetry/opamp/client/internal/request/delay/ExponentialBackoffPeriodicDelayTest.java
@@ -0,0 +1,24 @@
+package io.opentelemetry.opamp.client.internal.request.delay;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.Duration;
+import org.junit.jupiter.api.Test;
+
+class ExponentialBackoffPeriodicDelayTest {
+  @Test
+  void verifyDelayUpdates() {
+    ExponentialBackoffPeriodicDelay delay =
+        new ExponentialBackoffPeriodicDelay(Duration.ofSeconds(1));
+
+    assertThat(delay.getNextDelay()).isEqualTo(Duration.ofSeconds(1));
+    assertThat(delay.getNextDelay()).isEqualTo(Duration.ofSeconds(2));
+    assertThat(delay.getNextDelay()).isEqualTo(Duration.ofSeconds(4));
+    assertThat(delay.getNextDelay()).isEqualTo(Duration.ofSeconds(8));
+    assertThat(delay.getNextDelay()).isEqualTo(Duration.ofSeconds(16));
+
+    // Reset
+    delay.reset();
+    assertThat(delay.getNextDelay()).isEqualTo(Duration.ofSeconds(1));
+  }
+}
