524b826b031e419baa4dc8fad2ee691edbdbf32c
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2025-08-07 17:39:05 +0200

    Creating signal storage subtypes

--- disk-buffering/src/main/java/io/opentelemetry/contrib/disk/buffering/storage/Storage.java
+++ disk-buffering/src/main/java/io/opentelemetry/contrib/disk/buffering/storage/Storage.java
@@ -2,12 +2,21 @@ package io.opentelemetry.contrib.disk.buffering.storage;
 
 import io.opentelemetry.contrib.disk.buffering.storage.operations.ReadOperation;
 import io.opentelemetry.contrib.disk.buffering.storage.operations.WriteOperation;
+import io.opentelemetry.sdk.logs.data.LogRecordData;
+import io.opentelemetry.sdk.metrics.data.MetricData;
+import io.opentelemetry.sdk.trace.data.SpanData;
 import java.io.Closeable;
 import java.util.Collection;
 import java.util.concurrent.CompletableFuture;
 
-public interface SignalStorage<T> extends Closeable {
+public interface Storage<T> extends Closeable {
   CompletableFuture<WriteOperation> write(Collection<T> items);
 
   CompletableFuture<ReadOperation<T>> read();
+
+  interface Span extends Storage<SpanData> {}
+
+  interface LogRecord extends Storage<LogRecordData> {}
+
+  interface Metric extends Storage<MetricData> {}
 }
