bfe1c5e5bee538ce06b00d035fef62cd9ea85887
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-08-15 13:08:30 +0200

    update config bridge from agent

--- declarative-config-bridge/src/main/java/io/opentelemetry/contrib/sdk/autoconfigure/DeclarativeConfigPropertiesBridgeBuilder.java
+++ declarative-config-bridge/src/main/java/io/opentelemetry/contrib/sdk/autoconfigure/DeclarativeConfigPropertiesBridgeBuilder.java
@@ -13,6 +13,7 @@ import io.opentelemetry.api.incubator.config.DeclarativeConfigProperties;
 import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk;
 import io.opentelemetry.sdk.autoconfigure.internal.AutoConfigureUtil;
 import io.opentelemetry.sdk.autoconfigure.spi.ConfigProperties;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import javax.annotation.Nullable;
@@ -20,32 +21,49 @@ import javax.annotation.Nullable;
 /**
  * A builder for {@link DeclarativeConfigPropertiesBridge} that allows adding translations and fixed
  * values for properties.
- *
- * <p>This class is internal and is hence not for public use. Its APIs are unstable and can change
- * at any time.
  */
 public class DeclarativeConfigPropertiesBridgeBuilder {
-  private final LinkedHashMap<String, String> translationMap = new LinkedHashMap<>();
-  private final Map<String, Object> fixedValues = new LinkedHashMap<>();
+  /**
+   * order is important here, so we use LinkedHashMap - see {@link #addMapping(String, String)} for
+   * more details
+   */
+  private final Map<String, String> mappings = new LinkedHashMap<>();
+
+  private final Map<String, Object> overrideValues = new HashMap<>();
 
   public DeclarativeConfigPropertiesBridgeBuilder() {}
 
+  /**
+   * Adds a mapping from a property prefix to a YAML path.
+   *
+   * <p>For example, if the property prefix is "otel.javaagent" and the YAML path is "agent", then
+   * any property starting with "otel.javaagent." will be resolved against the "agent" node in the
+   * instrumentation/java section of the YAML configuration.
+   *
+   * @param propertyPrefix the prefix of the property to translate
+   * @param yamlPath the YAML path to resolve the property against
+   */
   @CanIgnoreReturnValue
-  public DeclarativeConfigPropertiesBridgeBuilder addTranslation(
-      String propertyName, String yamlPath) {
-    translationMap.put(propertyName, yamlPath);
+  public DeclarativeConfigPropertiesBridgeBuilder addMapping(
+      String propertyPrefix, String yamlPath) {
+    mappings.put(propertyPrefix, yamlPath);
     return this;
   }
 
+  /**
+   * Adds a fixed override value for a property.
+   *
+   * @param propertyName the name of the property to override
+   * @param value the value to return when the property is requested
+   */
   @CanIgnoreReturnValue
-  public DeclarativeConfigPropertiesBridgeBuilder addFixedValue(String propertyName, Object value) {
-    fixedValues.put(propertyName, value);
+  public DeclarativeConfigPropertiesBridgeBuilder addOverride(String propertyName, Object value) {
+    overrideValues.put(propertyName, value);
     return this;
   }
 
-  /** Resolve {@link ConfigProperties} from the {@code autoConfiguredOpenTelemetrySdk}. */
-  public ConfigProperties resolveConfigProperties(
-      AutoConfiguredOpenTelemetrySdk autoConfiguredOpenTelemetrySdk) {
+  /** Build {@link ConfigProperties} from the {@code autoConfiguredOpenTelemetrySdk}. */
+  public ConfigProperties build(AutoConfiguredOpenTelemetrySdk autoConfiguredOpenTelemetrySdk) {
     ConfigProperties sdkConfigProperties =
         AutoConfigureUtil.getConfig(autoConfiguredOpenTelemetrySdk);
     if (sdkConfigProperties != null) {
@@ -54,23 +72,31 @@ public class DeclarativeConfigPropertiesBridgeBuilder {
     ConfigProvider configProvider =
         AutoConfigureUtil.getConfigProvider(autoConfiguredOpenTelemetrySdk);
     if (configProvider != null) {
-      return resolveInstrumentationConfig(configProvider.getInstrumentationConfig());
+      return buildFromInstrumentationConfig(configProvider.getInstrumentationConfig());
     }
     // Should never happen
     throw new IllegalStateException(
         "AutoConfiguredOpenTelemetrySdk does not have ConfigProperties or DeclarativeConfigProperties. This is likely a programming error in opentelemetry-java");
   }
 
-  public ConfigProperties resolveInstrumentationConfig(
+  /**
+   * Build {@link ConfigProperties} from the {@link DeclarativeConfigProperties} provided by the
+   * instrumentation configuration.
+   *
+   * <p>If the provided {@code instrumentationConfig} is null, an empty {@link
+   * DeclarativeConfigProperties} will be used.
+   *
+   * @param instrumentationConfig the instrumentation configuration to build from
+   * @return a new instance of {@link ConfigProperties}
+   */
+  public ConfigProperties buildFromInstrumentationConfig(
       @Nullable DeclarativeConfigProperties instrumentationConfig) {
+    // leave the name "build" for a future method that builds from a DeclarativeConfigProperties
+    // instance that doesn't come from the top-level instrumentation config
     if (instrumentationConfig == null) {
       instrumentationConfig = DeclarativeConfigProperties.empty();
     }
-    return resolveConfig(instrumentationConfig.getStructured("java", empty()));
-  }
-
-  public ConfigProperties resolveConfig(@Nullable DeclarativeConfigProperties config) {
     return new DeclarativeConfigPropertiesBridge(
-        config, new ConfigPropertiesTranslator(translationMap, fixedValues));
+        instrumentationConfig.getStructured("java", empty()), mappings, overrideValues);
   }
 }
