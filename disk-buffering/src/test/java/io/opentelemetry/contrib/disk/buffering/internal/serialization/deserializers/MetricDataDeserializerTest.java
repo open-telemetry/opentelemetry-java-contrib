8a00ac8414fc321445d5d1cfc352d110237b0342
jacekpietras <jpietras@spoton.com>
2025-08-22 15:24:25 +0200

    Catching IllegalStateException in case of failed deserialization

---
+++ disk-buffering/src/test/java/io/opentelemetry/contrib/disk/buffering/internal/serialization/deserializers/MetricDataDeserializerTest.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.contrib.disk.buffering.internal.serialization.deserializers;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import io.opentelemetry.contrib.disk.buffering.internal.serialization.serializers.SignalSerializer;
+import io.opentelemetry.contrib.disk.buffering.testutils.BaseSignalSerializerTest;
+import io.opentelemetry.contrib.disk.buffering.testutils.TestData;
+import io.opentelemetry.sdk.metrics.data.MetricData;
+import org.junit.jupiter.api.Test;
+
+class MetricDataDeserializerTest extends BaseSignalSerializerTest<MetricData> {
+
+    @Test
+    void whenDecodingMalformedMessage_wrapIntoDeserializationException() {
+        assertThrows(
+                DeserializationException.class,
+                () -> getDeserializer().deserialize(TestData.makeMalformedSignalBinary())
+        );
+    }
+
+    @Test
+    void whenDecodingTooShortMessage_wrapIntoDeserializationException() {
+        assertThrows(
+                DeserializationException.class,
+                () -> getDeserializer().deserialize(TestData.makeTooShortSignalBinary())
+        );
+    }
+
+    @Override
+    protected SignalSerializer<MetricData> getSerializer() {
+        return SignalSerializer.ofMetrics();
+    }
+
+    @Override
+    protected SignalDeserializer<MetricData> getDeserializer() {
+        return SignalDeserializer.ofMetrics();
+    }
+}
---
+++ disk-buffering/src/test/java/io/opentelemetry/contrib/disk/buffering/internal/serialization/deserializers/SpanDataDeserializerTest.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.contrib.disk.buffering.internal.serialization.deserializers;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import io.opentelemetry.api.trace.SpanKind;
+import io.opentelemetry.contrib.disk.buffering.internal.serialization.mapping.spans.models.SpanDataImpl;
+import io.opentelemetry.contrib.disk.buffering.internal.serialization.serializers.SignalSerializer;
+import io.opentelemetry.contrib.disk.buffering.testutils.BaseSignalSerializerTest;
+import io.opentelemetry.contrib.disk.buffering.testutils.TestData;
+import io.opentelemetry.sdk.trace.data.SpanData;
+import io.opentelemetry.sdk.trace.data.StatusData;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+
+class SpanDataDeserializerTest extends BaseSignalSerializerTest<SpanData> {
+    private static final SpanData SPAN_DATA = SpanDataImpl.builder()
+            .setSpanContext(TestData.SPAN_CONTEXT)
+            .setParentSpanContext(TestData.PARENT_SPAN_CONTEXT)
+            .setName("Test span")
+            .setKind(SpanKind.SERVER)
+            .setStartEpochNanos(100L)
+            .setEndEpochNanos(200L)
+            .setStatus(StatusData.ok())
+            .setAttributes(TestData.ATTRIBUTES)
+            .setResource(TestData.RESOURCE_FULL)
+            .setInstrumentationScopeInfo(TestData.INSTRUMENTATION_SCOPE_INFO_FULL)
+            .setTotalRecordedLinks(0)
+            .setTotalRecordedEvents(0)
+            .setTotalAttributeCount(0)
+            .setEvents(Collections.emptyList())
+            .setLinks(Collections.emptyList())
+            .build();
+
+    @Test
+    void verifyDeserialization() {
+        assertSerializeDeserialize(SPAN_DATA, SPAN_DATA);
+    }
+
+    @Test
+    void whenDecodingMalformedMessage_wrapIntoDeserializationException() {
+        assertThrows(
+                DeserializationException.class,
+                () -> getDeserializer().deserialize(TestData.makeMalformedSignalBinary())
+        );
+    }
+
+    @Test
+    void whenDecodingTooShortMessage_wrapIntoDeserializationException() {
+        assertThrows(
+                DeserializationException.class,
+                () -> getDeserializer().deserialize(TestData.makeTooShortSignalBinary())
+        );
+    }
+
+    @Override
+    protected SignalSerializer<SpanData> getSerializer() {
+        return SignalSerializer.ofSpans();
+    }
+
+    @Override
+    protected SignalDeserializer<SpanData> getDeserializer() {
+        return SignalDeserializer.ofSpans();
+    }
+}
