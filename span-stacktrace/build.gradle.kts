741de0f188b454643951b9ccb3449181cee53256
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-09-15 11:17:11 +0200

    update

--- span-stacktrace/build.gradle.kts
+++ span-stacktrace/build.gradle.kts
@@ -17,11 +17,11 @@ dependencies {
   compileOnly("io.opentelemetry:opentelemetry-sdk-extension-autoconfigure-spi")
   compileOnly("io.opentelemetry:opentelemetry-sdk-extension-autoconfigure")
   compileOnly("io.opentelemetry:opentelemetry-sdk-extension-incubator")
-  compileOnly("io.opentelemetry.instrumentation:opentelemetry-instrumentation-api-incubator")
+  compileOnly("io.opentelemetry.instrumentation:opentelemetry-declarative-config-bridge")
   testImplementation("io.opentelemetry:opentelemetry-sdk-extension-autoconfigure")
   testImplementation("io.opentelemetry:opentelemetry-sdk-extension-autoconfigure-spi")
   testImplementation("io.opentelemetry:opentelemetry-sdk-extension-incubator")
-  testImplementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-api-incubator")
+  testImplementation("io.opentelemetry.instrumentation:opentelemetry-declarative-config-bridge")
 
   compileOnly("io.opentelemetry.semconv:opentelemetry-semconv")
   testImplementation("io.opentelemetry.semconv:opentelemetry-semconv")
@@ -30,12 +30,4 @@ dependencies {
   testCompileOnly("com.google.auto.service:auto-service-annotations")
 
   testImplementation("io.opentelemetry:opentelemetry-exporter-logging")
-}
-
-// todo remove when https://github.com/open-telemetry/opentelemetry-java-instrumentation/pull/14497 is merged
-configurations.all {
-  resolutionStrategy {
-    force("io.opentelemetry.instrumentation:opentelemetry-instrumentation-api-incubator:2.20.0-alpha-SNAPSHOT")
-    force("io.opentelemetry.instrumentation:opentelemetry-instrumentation-api:2.19.0")
-  }
 }
