0b10c3171297dd8ea209c63a76542dfb46669b8a
Trask Stalnaker <trask.stalnaker@gmail.com>
2025-09-22 16:50:45 -0700

    Update gradle plugin format

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,13 +12,13 @@ repositories {
 
 dependencies {
   // When updating, update above in plugins too
-  implementation("com.diffplug.spotless:spotless-plugin-gradle:7.2.1")
-  implementation("net.ltgt.gradle:gradle-errorprone-plugin:4.3.0")
-  implementation("net.ltgt.gradle:gradle-nullaway-plugin:2.3.0")
-  implementation("org.owasp:dependency-check-gradle:12.1.5")
+  implementation("com.diffplug.spotless:com.diffplug.spotless.gradle.plugin:7.2.1")
+  implementation("net.ltgt.errorprone:net.ltgt.errorprone.gradle.plugin:4.3.0")
+  implementation("net.ltgt.nullaway:net.ltgt.nullaway.gradle.plugin:2.3.0")
+  implementation("org.owasp.dependencycheck:org.owasp.dependencycheck.gradle.plugin:12.1.5")
   implementation("ru.vyarus.animalsniffer:ru.vyarus.animalsniffer.gradle.plugin:2.0.1")
-  implementation("com.gradle:develocity-gradle-plugin:4.2")
-  implementation("me.champeau.gradle:japicmp-gradle-plugin:0.4.6")
+  implementation("com.gradle.develocity:com.gradle.develocity.gradle.plugin:4.2")
+  implementation("me.champeau.gradle.japicmp:me.champeau.gradle.japicmp.gradle.plugin:0.4.6")
   implementation("com.google.auto.value:auto-value-annotations:1.11.0")
 }
 
