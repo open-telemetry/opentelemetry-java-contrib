70b7dc128d832f39cb5539d6037c547cd998b7e8
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-09-30 22:54:20 +0200

    Use mise to check for modified files (#2318)
    Co-authored-by: Trask Stalnaker <trask.stalnaker@gmail.com>

---
+++ .mise/tasks/lint/local-links.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+#MISE description="Lint links in all files"
+
+set -e
+
+#USAGE arg "<file>" var=#true help="files to check" default="."
+
+lychee --verbose --scheme file --include-fragments "$usage_file"
