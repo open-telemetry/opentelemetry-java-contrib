3a65310b85adfe36b38d4a459e3214de98427900
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-10-10 09:11:04 +0200

    Fix lychee (#2345)

--- .mise/tasks/lint/links-in-modified-files.sh
+++ .mise/tasks/lint/links-in-modified-files.sh
@@ -5,6 +5,7 @@ set -e
 
 #USAGE flag "--base <base>" help="base branch to compare against (default: origin/main)" default="origin/main"
 #USAGE flag "--head <head>" help="head branch to compare against (empty for local changes) (default: empty)" default=""
+#USAGE flag "--event <event>" help="event name (default: pull_request)" default="pull_request"
 
 if [ "$usage_head" = "''" ]; then
   usage_head=""
@@ -16,7 +17,10 @@ fi
 config_modified=$(git diff --name-only --merge-base "$usage_base" $usage_head \
                   | grep -E '^(\.github/config/lychee\.toml|\.mise/tasks/lint/.*|mise\.toml)$' || true)
 
-if [ -n "$config_modified" ] ; then
+if [ "$usage_event" != "pull_request" ] ; then
+  echo "Not a PR - checking all files."
+  mise run lint:links
+elif [ -n "$config_modified" ] ; then
   echo "config changes, checking all files."
   mise run lint:links
 else
