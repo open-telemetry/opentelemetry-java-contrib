dc40365558f98ff4a3a96aae7561d67834053121
Trask Stalnaker <trask.stalnaker@gmail.com>
2025-10-10 16:56:28 -0700

    up

--- jmx-scraper/src/integrationTest/java/io/opentelemetry/contrib/jmxscraper/JmxConnectionTest.java
+++ jmx-scraper/src/integrationTest/java/io/opentelemetry/contrib/jmxscraper/JmxConnectionTest.java
@@ -15,33 +15,23 @@ import java.util.function.Function;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.EnabledIf;
 import org.junit.jupiter.api.io.TempDir;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.testcontainers.DockerClientFactory;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.Network;
 import org.testcontainers.containers.output.Slf4jLogConsumer;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 /**
  * Tests all supported ways to connect to remote JMX interface. This indirectly tests
  * JmxConnectionBuilder and relies on containers to minimize the JMX/RMI network complications which
  * are not NAT-friendly.
  */
-@EnabledIf("isDockerAvailable")
+@Testcontainers(disabledWithoutDocker = true)
 class JmxConnectionTest {
-
-  static boolean isDockerAvailable() {
-    try {
-      DockerClientFactory.instance().client();
-      return true;
-    } catch (Throwable e) {
-      return false;
-    }
-  }
 
   // OTLP endpoint is not used in test mode, but still has to be provided
   private static final String DUMMY_OTLP_ENDPOINT = "http://dummy-otlp-endpoint:8080/";
