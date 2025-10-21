ca0004fe0433155f0eba208b6861caf53c8595bf
Trask Stalnaker <trask.stalnaker@gmail.com>
2025-10-21 15:33:53 -0700

    Fix gradle deprecation warning

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,11 +3,13 @@ pluginManagement {
     id("com.gradleup.shadow") version "9.2.2"
     id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
     id("com.gradle.develocity") version "4.2.2"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
   }
 }
 
 plugins {
   id("com.gradle.develocity")
+  id("org.gradle.toolchains.foojay-resolver-convention")
 }
 
 dependencyResolutionManagement {
