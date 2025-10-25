36435d4e691eb0a9d48de030ddc39f1954314036
Dominic Lüchinger <doldev@snowgarden.ch>
2025-10-25 16:12:19 +0200

    chore: Added more project links to the pom.xml
    According to https://maven.apache.org/pom.html#SCM the `scm.url` should
    be a publicly browsable repository.
    Replaced git URI with URL to project page on Github.
    
    Added `issueManagement` and `ciManagement` information to the scm
    section.

--- version.gradle.kts
+++ version.gradle.kts
@@ -1,5 +1,6 @@
 val stableVersion = "1.52.0-SNAPSHOT"
 val alphaVersion = "1.52.0-alpha-SNAPSHOT"
+val tagVersion by extra { "v$stableVersion" }
 
 allprojects {
   if (findProperty("otel.stable") != "true") {
