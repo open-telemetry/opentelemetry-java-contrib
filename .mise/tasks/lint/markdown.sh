8241e18586a6a6a64d092bc653de57bd0f40a9ea
Trask Stalnaker <trask.stalnaker@gmail.com>
2025-11-25 12:58:14 -0800

    ignores

--- .mise/tasks/lint/markdown.sh
+++ .mise/tasks/lint/markdown.sh
@@ -5,7 +5,7 @@
 set -e
 
 if [ "${usage_fix}" = "true" ]; then
-  markdownlint-cli2 --fix "**/*.md"
-else
-  markdownlint-cli2 "**/*.md"
+  opt_fix="--fix"
 fi
+
+markdownlint-cli2 ${opt_fix} "**/*.md" "#**/build" "#**/node_modules" "#CHANGELOG.md" "#ibm-mq-metrics/docs/metrics.md" "#.github/pull_request_template.md"
