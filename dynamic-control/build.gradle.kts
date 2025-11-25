026fbb479bc7018438deb03420a169d6c82ab186
jackshirazi <jack.shirazi@elastic.co>
2025-11-25 15:28:59 +0000

    Add skeleton extension to dynamic-control (#2470)

--- dynamic-control/build.gradle.kts
+++ dynamic-control/build.gradle.kts
@@ -11,3 +11,22 @@ java {
   sourceCompatibility = JavaVersion.VERSION_1_8
   targetCompatibility = JavaVersion.VERSION_1_8
 }
+
+dependencies {
+  annotationProcessor("com.google.auto.service:auto-service")
+  compileOnly("com.google.auto.service:auto-service-annotations")
+
+  compileOnly("io.opentelemetry:opentelemetry-sdk-extension-autoconfigure")
+  compileOnly("io.opentelemetry:opentelemetry-sdk-extension-autoconfigure-spi")
+
+  testCompileOnly("com.google.auto.service:auto-service-annotations")
+  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+  testImplementation("org.junit.jupiter:junit-jupiter-api")
+  testCompileOnly("org.junit.jupiter:junit-jupiter-params")
+
+  testImplementation("io.opentelemetry:opentelemetry-sdk-extension-autoconfigure")
+  testImplementation("io.opentelemetry:opentelemetry-sdk-extension-autoconfigure-spi")
+  testImplementation("org.assertj:assertj-core")
+  testImplementation("org.mockito:mockito-inline")
+  testImplementation("org.mockito:mockito-junit-jupiter")
+}
---
+++ dynamic-control/src/test/java/io/opentelemetry/contrib/dynamic/DynamicControlAutoConfigurationTest.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.contrib.dynamic;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+import io.opentelemetry.sdk.autoconfigure.spi.AutoConfigurationCustomizer;
+import org.junit.jupiter.api.Test;
+
+class DynamicControlAutoConfigurationTest {
+
+  @Test
+  void testCustomize() {
+    DynamicControlAutoConfiguration config = new DynamicControlAutoConfiguration();
+    AutoConfigurationCustomizer customizer = mock(AutoConfigurationCustomizer.class);
+
+    config.customize(customizer);
+
+    // The customize method should not throw and should be callable
+    // Logging is tested manually or via integration tests
+  }
+
+  @Test
+  void testOrder() {
+    // This is a placeholder test, just to have something
+    DynamicControlAutoConfiguration config = new DynamicControlAutoConfiguration();
+    // Default order should be 0
+    assertThat(config.order()).isEqualTo(0);
+  }
+}
