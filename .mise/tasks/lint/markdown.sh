c997f5eaf240199289200eeeb3363be17bff1801
Trask Stalnaker <trask.stalnaker@gmail.com>
2025-12-02 07:29:16 -0800

    Fix markdown fix
    As recommended in https://mise.jdx.dev/tasks/file-tasks.html#example-file-task-with-arguments

--- .mise/tasks/lint/markdown.sh
+++ .mise/tasks/lint/markdown.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 #MISE description="Lint markdown files"
-#MISE flag "--fix" help="Automatically fix issues"
+#USAGE flag "--fix" help="Automatically fix issues"
 
 set -e
 
-if [ "${usage_fix}" = "true" ]; then
+if [ "${usage_fix:-false}" = "true" ]; then
   markdownlint-cli2 --fix "**/*.md" "#**/build" "#**/node_modules" "#CHANGELOG.md" "#ibm-mq-metrics/docs/metrics.md" "#.github/pull_request_template.md"
 else
   markdownlint-cli2 "**/*.md" "#**/build" "#**/node_modules" "#CHANGELOG.md" "#ibm-mq-metrics/docs/metrics.md" "#.github/pull_request_template.md"
