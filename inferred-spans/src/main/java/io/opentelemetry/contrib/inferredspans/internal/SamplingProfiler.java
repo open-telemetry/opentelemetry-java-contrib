30aa33ad702bfedc13a16c0c73e4d5be09231a1f
Jack Shirazi <jack.shirazi@elastic.co>
2025-12-08 15:51:57 +0000

    end previous session quicker

--- inferred-spans/src/main/java/io/opentelemetry/contrib/inferredspans/internal/SamplingProfiler.java
+++ inferred-spans/src/main/java/io/opentelemetry/contrib/inferredspans/internal/SamplingProfiler.java
@@ -766,6 +766,9 @@ public class SamplingProfiler implements Runnable {
 
   public void stop() throws InterruptedException, IOException {
     // cancels/interrupts the profiling thread
+    if (profilingTask != null) {
+      profilingTask.cancel(true);
+    }
     // implicitly clears profiled threads
     scheduler.shutdown();
     scheduler.awaitTermination(10, TimeUnit.SECONDS);
