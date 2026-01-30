6770c04c14e5d6f4edd9b0314b1056977a052ed9
Dmytro Shkil <d.shkil@gmail.com>
2026-01-30 19:37:32 +0200

    Fix IllegalArgumentException: Unrecognized AnyValue type on empty LogRecord.body (#2529)

--- disk-buffering/src/test/java/io/opentelemetry/contrib/disk/buffering/internal/serialization/mapping/logs/LogRecordDataMapperTest.java
+++ disk-buffering/src/test/java/io/opentelemetry/contrib/disk/buffering/internal/serialization/mapping/logs/LogRecordDataMapperTest.java
@@ -11,10 +11,12 @@ import io.opentelemetry.api.common.Value;
 import io.opentelemetry.api.logs.Severity;
 import io.opentelemetry.contrib.disk.buffering.internal.serialization.mapping.logs.models.LogRecordDataImpl;
 import io.opentelemetry.contrib.disk.buffering.testutils.TestData;
+import io.opentelemetry.proto.common.v1.AnyValue;
 import io.opentelemetry.proto.logs.v1.LogRecord;
 import io.opentelemetry.sdk.common.InstrumentationScopeInfo;
 import io.opentelemetry.sdk.logs.data.LogRecordData;
 import io.opentelemetry.sdk.resources.Resource;
+import java.io.IOException;
 import org.junit.jupiter.api.Test;
 
 class LogRecordDataMapperTest {
@@ -34,12 +36,48 @@ class LogRecordDataMapperTest {
           .setEventName("my.event.name")
           .build();
 
+  private static final LogRecordData LOG_RECORD_WITH_EMPTY_STRING_BODY =
+      LogRecordDataImpl.builder()
+          .setResource(TestData.RESOURCE_FULL)
+          .setSpanContext(TestData.SPAN_CONTEXT)
+          .setInstrumentationScopeInfo(TestData.INSTRUMENTATION_SCOPE_INFO_FULL)
+          .setAttributes(TestData.ATTRIBUTES)
+          .setBodyValue(Value.of(""))
+          .setSeverity(Severity.DEBUG)
+          .setSeverityText("Log severity text")
+          .setTimestampEpochNanos(100L)
+          .setObservedTimestampEpochNanos(200L)
+          .setTotalAttributeCount(3)
+          .setEventName("my.event.name")
+          .build();
+
   @Test
-  void verifyMapping() {
-    LogRecord proto = mapToProto(LOG_RECORD);
+  void verifyMapping() throws IOException {
+    LogRecord proto = encodeAndDecode(mapToProto(LOG_RECORD));
 
     assertThat(mapToSdk(proto, LOG_RECORD.getResource(), LOG_RECORD.getInstrumentationScopeInfo()))
         .isEqualTo(LOG_RECORD);
+  }
+
+  @Test
+  void verifyEmptyBodyMapping() throws IOException {
+    LogRecord proto = encodeAndDecode(mapToProto(LOG_RECORD_WITH_EMPTY_STRING_BODY));
+
+    LogRecord emptyBodyProto =
+        proto
+            .newBuilder()
+            // It can't be replicated in tests, but in production, after decoding the protobuf,
+            // we ended up with an empty AnyValue when the original body was an empty string.
+            // So we are faking it.
+            .body(new AnyValue.Builder().build())
+            .build();
+
+    assertThat(
+            mapToSdk(
+                emptyBodyProto,
+                LOG_RECORD_WITH_EMPTY_STRING_BODY.getResource(),
+                LOG_RECORD_WITH_EMPTY_STRING_BODY.getInstrumentationScopeInfo()))
+        .isEqualTo(LOG_RECORD_WITH_EMPTY_STRING_BODY);
   }
 
   private static LogRecord mapToProto(LogRecordData data) {
@@ -49,5 +87,9 @@ class LogRecordDataMapperTest {
   private static LogRecordData mapToSdk(
       LogRecord data, Resource resource, InstrumentationScopeInfo scopeInfo) {
     return LogRecordDataMapper.getInstance().mapToSdk(data, resource, scopeInfo);
+  }
+
+  private static LogRecord encodeAndDecode(LogRecord proto) throws IOException {
+    return LogRecord.ADAPTER.decode(proto.encode());
   }
 }
