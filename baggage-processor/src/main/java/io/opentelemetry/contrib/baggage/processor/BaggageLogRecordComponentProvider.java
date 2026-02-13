be56f5b8d8736788777b8d3c69e476661f06e318
Jay DeLuca <jaydeluca4@gmail.com>
2026-02-13 07:22:28 -0500

    update imports

--- baggage-processor/src/main/java/io/opentelemetry/contrib/baggage/processor/BaggageLogRecordComponentProvider.java
+++ baggage-processor/src/main/java/io/opentelemetry/contrib/baggage/processor/BaggageLogRecordComponentProvider.java
@@ -8,7 +8,7 @@ package io.opentelemetry.contrib.baggage.processor;
 import com.google.auto.service.AutoService;
 import io.opentelemetry.api.incubator.config.DeclarativeConfigProperties;
 import io.opentelemetry.sdk.autoconfigure.spi.internal.ComponentProvider;
-import io.opentelemetry.sdk.internal.IncludeExcludePredicate;
+import io.opentelemetry.sdk.common.internal.IncludeExcludePredicate;
 import io.opentelemetry.sdk.logs.LogRecordProcessor;
 
 @AutoService(ComponentProvider.class)
