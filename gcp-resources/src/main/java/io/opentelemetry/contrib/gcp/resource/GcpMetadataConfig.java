893b6a67f3c5213527383e03b11bfbfdc169d0f8
Pranav Sharma <sharmapranav@google.com>
2026-04-07 19:12:03 +0000

    Address lint issue

--- gcp-resources/src/main/java/io/opentelemetry/contrib/gcp/resource/GcpMetadataConfig.java
+++ gcp-resources/src/main/java/io/opentelemetry/contrib/gcp/resource/GcpMetadataConfig.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.URI;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
@@ -143,7 +144,7 @@ final class GcpMetadataConfig {
   @Nullable
   private String fetchAttribute(String attributeName) {
     try {
-      URL url = new URL(this.url + attributeName);
+      URL url = URI.create(this.url + attributeName).toURL();
       HttpURLConnection connection = (HttpURLConnection) url.openConnection();
       connection.setRequestProperty("Metadata-Flavor", "Google");
       if (connection.getResponseCode() == 200
