fb197aa301ff930cc487a452288bffbca1e6d69c
Antoine Toulme <atoulme@splunk.com>
2026-05-19 17:58:51 -0700

    errorprone

--- ibm-mq-metrics/src/main/java/io/opentelemetry/ibm/mq/util/MetricsUtil.java
+++ ibm-mq-metrics/src/main/java/io/opentelemetry/ibm/mq/util/MetricsUtil.java
@@ -7,7 +7,9 @@ package io.opentelemetry.ibm.mq.util;
 
 import java.util.function.Function;
 
-public class MetricsUtil {
+public final class MetricsUtil {
+
+  private MetricsUtil() {}
 
   public static final Function<Integer, Long> MIBY_TO_BYTES = x -> x * 1024L * 1024L;
 }
